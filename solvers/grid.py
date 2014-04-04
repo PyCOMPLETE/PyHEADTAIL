8a3122ab77a70e636d2b1fd2c063f94eb8d4d2ba
Kevin Shing Bruce Li <Kevin.Shing.Bruce.Li@cern.ch>
2014-04-04 21:25:00 +0200

    Implement density computation using histogram.

--- solvers/grid.py
+++ solvers/grid.py
@@ -51,28 +51,34 @@ class UniformGrid(object):
         # Initialise
         self.rho[:] = 0
 
-        # On regular mesh
-        dx = self.x[0, 1] - self.x[0, 0]
-        dy = self.y[1, 0] - self.y[0, 0]
-        dxi = 1 / dx
-        dyi = 1 / dy
-        ai = 1 / (dx * dy)
-        # TODO: on adaptive mesh
+        # # On regular mesh
+        # dx = self.x[0, 1] - self.x[0, 0]
+        # dy = self.y[1, 0] - self.y[0, 0]
+        # dxi = 1 / dx
+        # dyi = 1 / dy
+        # ai = 1 / (dx * dy)
+        # # TODO: on adaptive mesh
 
-        # Line charge density and particle selection
-        # double lambda;
-        # std::vector<int> index;
-        # t.get_slice(i_slice, lambda, index);
-        # int np = index.size();
-        l = 1
-        x, y = bunch.x, bunch.y
+        # # Line charge density and particle selection
+        # # double lambda;
+        # # std::vector<int> index;
+        # # t.get_slice(i_slice, lambda, index);
+        # # int np = index.size();
+        # l = 1
+        # x, y = bunch.x, bunch.y
 
-        fx, fy = (x - self.x[0,0]) * dxi, (y - self.y[0,0]) * dyi
-        ix, iy = np.floor(fx).astype(int), np.floor(fy).astype(int)
-        fx, fy = fx - ix, fy - iy
+        # fx, fy = (x - self.x[0,0]) * dxi, (y - self.y[0,0]) * dyi
+        # ix, iy = np.floor(fx).astype(int), np.floor(fy).astype(int)
+        # fx, fy = fx - ix, fy - iy
 
-        H, xedges, yedges = np.histogram2d(ix, iy, bins=self.rho.shape)
-        self.rho += H
+        # H, xedges, yedges = np.histogram2d(ix, iy, bins=self.rho.shape)
+        x0, x1 = self.x[0,0] - self.dx / 2, self.x[0,-1] + self.dx / 2
+        y0, y1 = self.y[0,0] - self.dy / 2, self.y[-1,0] + self.dy / 2
+        xedges = np.linspace(x0, x1, self.nx + 1)
+        yedges = np.linspace(y0, y1, self.ny + 1)
+        H, xedges, yedges = np.histogram2d(bunch.x, bunch.y, bins=(xedges, yedges))
+
+        self.rho += H.T
 
     @profile
     def track(self, bunch):
@@ -81,7 +87,8 @@ class UniformGrid(object):
 
         for i in xrange(bunch.slices.n_slices):
             self.gather(bunch, i)
-        ax1.contourf(self.x, self.y, self.rho, 100)
+        print self.x.shape, self.y.shape, self.rho.shape
+        ax1.contourf(self.x, self.y, self.rho.T.T, 100)
         ax1.scatter(bunch.x, bunch.y, c='y', marker='.', alpha=0.1, lw=0)
         ax1.set_aspect('equal')
 
@@ -92,6 +99,7 @@ class UniformGrid(object):
         ax2.set_aspect('equal')
 
         plt.show()
+        exit(-1)
 
 # template<typename T, typename U>
 # void PoissonBase::fastscatter(T t, U u, int i_slice)
