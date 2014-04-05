e8253764f2e583fefa42a3b37ba38aba1822e7a1
Kevin Shing Bruce Li <Kevin.Shing.Bruce.Li@cern.ch>
2014-04-05 23:18:12 +0200

    Extend beam module by base and new derived classes.

--- beams/bunch.py
+++ beams/bunch.py
@@ -8,16 +8,69 @@ Created on 06.01.2014
 import numpy as np
 
 
-import h5py, sys
 from abc import ABCMeta, abstractmethod
-from scipy.constants import c, e
+from scipy.constants import c, e, m_e
+import h5py, sys
 from beams.slices import *
 from beams.matching import match_transverse, match_longitudinal, unmatched_inbucket
 
 
-class BeamBase(object):
+class BaseBeam(object):
 
     __metaclass__ = ABCMeta
+
+    def __init__(self, x, xp, y, yp, dz, dp):
+
+        assert(len(x) == len(xp) == len(y) == len(yp) == len(dz) == len(dp))
+
+        self.x = x
+        self.xp = xp
+        self.y = y
+        self.yp = yp
+        self.dz = dz
+        self.dp = dp
+
+    @classmethod
+    def from_empty(self, n_macroparticles):
+
+        x = np.zeros(n_macroparticles)
+        xp = np.zeros(n_macroparticles)
+        y = np.zeros(n_macroparticles)
+        yp = np.zeros(n_macroparticles)
+        dz = np.zeros(n_macroparticles)
+        dp = np.zeros(n_macroparticles)
+
+        self = cls(x, xp, y, yp, dz, dp)
+
+        return self
+
+    @classmethod
+    def from_gauss(self, n_macroparticles):
+
+        x = np.random.randn(n_macroparticles)
+        xp = np.random.randn(n_macroparticles)
+        y = np.random.randn(n_macroparticles)
+        yp = np.random.randn(n_macroparticles)
+        dz = np.random.randn(n_macroparticles)
+        dp = np.random.randn(n_macroparticles)
+
+        self = cls(x, xp, y, yp, dz, dp)
+
+        return self
+
+    @classmethod
+    def from_uniform(self, n_macroparticles):
+
+        x = 2 * np.random.rand(n_macroparticles) - 1
+        xp = 2 * np.random.rand(n_macroparticles) - 1
+        y = 2 * np.random.rand(n_macroparticles) - 1
+        yp = 2 * np.random.rand(n_macroparticles) - 1
+        dz = 2 * np.random.rand(n_macroparticles) - 1
+        dp = 2 * np.random.rand(n_macroparticles) - 1
+
+        self = cls(x, xp, y, yp, dz, dp)
+
+        return self
 
     @abstractmethod
     def set_beam_physics(self): pass
@@ -25,17 +78,178 @@ class BeamBase(object):
     @abstractmethod
     def set_beam_numerics(self): pass
 
-class Cloud(object):
 
-    def set_beam_physics(self, density, charge, mass): pass
+class Beam(BaseBeam):
 
-    def set_beam_numerics(self): pass
+    pass
+
+
+class Cloud(BaseBeam):
+
+    @classmethod
+    def from_file(self): pass
+
+    @classmethod
+    def from_parameters(n_macroparticles, density, extent_x, extent_y, extent_z):
+
+        self = cls.from_uniform(n_macroparticles)
+
+        self.x *= extent_x
+        self.xp *= 0
+        self.y *= extent_y
+        self.yp *= 0
+        self.dz *= 0
+        self.dp *= 0
+
+        self.x0 = self.x
+        self.xp0 = self.xp
+        self.y0 = self.yp
+        self.yp0 = self.yp
+
+        return self
+
+    def set_beam_physics(self, density, extent_x, extent_y, extent_z):
+
+        self.n_particles = density * extent_x * extent_y * extent_z
+        self.charge = e
+        self.gamma = 1
+        self.beta = np.sqrt(1 - 1 / self.gamma ** 2)
+        self.mass = m_e
+        self.p0 = mass * self.gamma * self.beta * c
+
+    def set_beam_numerics(self):
+
+        self.n_macroparticles = len(self.x)
+
+        self.id = np.arange(1, len(x) + 1)
+        self.np = np.ones(n_macroparticles) * self.n_particles / self.n_macroparticles
+
+    def reinitialize(self):
+
+        self.x = self.x0
+        self.xp = self.xp0
+        self.y = self.y0
+        self.yp = self.yp0
+
+    def add_poisson_solver(self, nx, ny): pass
+
+// Privates
+void Cloud::match_distribution(double extent_x, double extent_y)
+{
+    // Assignment
+    qsgn = (density > 0) - (density < 0);
+
+    //    const PoissonFFT poisson(128, 128);
+    //    this->poisson = poisson;
+
+    // Cloud dimensions
+    min_x = -std::max(std::abs(*std::min_element(x0.begin(), x0.end())),
+             std::abs(*std::max_element(x0.begin(), x0.end())));
+    max_x = -min_x;
+    min_y = -std::max(std::abs(*std::min_element(y0.begin(), y0.end())),
+             std::abs(*std::max_element(y0.begin(), y0.end())));
+    max_y = -min_y;
+}
+
+    void track(Bunch& bunch);
+    void rebuild(const Bunch& bunch);
+    void get_slice(int i_slice, double& lambda, std::vector<int>& index);
+
+    int qsgn;//#
+
+    //@{
+    double density;
+    double length;
+    double min_x;
+    double max_x;
+    double min_y;
+    double max_y;
+    //@}
+};
+
+void Cloud::get_slice(int i_slice, double& lambda, std::vector<int>& index)
+{
+    lambda = density / get_nparticles() * (max_x - min_x) * (max_y - min_y);
+    index.resize(get_nparticles());
+    for (int i=0; i<get_nparticles(); i++)
+        index[i] = i;
+}
+
+void Cloud::push(Bunch& bunch, int i_slice)
+{
+//    double t1 = omp_get_wtime();
+
+    // Normalization factors to speed up computations
+    const double dz = bunch.slice_dz[i_slice + 1] - bunch.slice_dz[i_slice];
+    const double dt = dz / (bunch.beta * c);
+    const double c_e = -re * 2 * c / bunch.beta * dz;
+    const double c_p = -rp * 2 / (bunch.gamma * bunch.beta * bunch.beta) * length;
+
+    // Line charge density and particle selection
+    double lambda;
+    std::vector<int> index;
+    bunch.get_slice(i_slice, lambda, index);
+    int np_e = get_nparticles();
+    int np_p = index.size();
+
+//    t1 = omp_get_wtime();
+    // Push bunch
+//#   pragma omp parallel for num_threads(2)
+    for (int j=0; j<np_p; j++)
+    {
+        int k = index[j];
+        bunch.xp[k] += c_p * bunch.kx[k];
+        bunch.yp[k] += c_p * bunch.ky[k];
+    }
+
+    // Push cloud
+//#   pragma omp parallel for num_threads(2) //schedule(dynamic)
+    for (int j=0; j<np_e; j++)
+    {
+        xp[j] += c_e * kx[j];
+        yp[j] += c_e * ky[j];
+        x[j] += xp[j] * dt;
+        y[j] += yp[j] * dt;
+    }
+//    t0 += omp_get_wtime() - t1;
+}
+
+void Cloud::track(Bunch& bunch)
+{
+    t0 = 0;
+    t1 = omp_get_wtime();
+    if (bunch.is_sliced)
+    {
+        this->rebuild(bunch);
+        poisson.initialize<Bunch&, Cloud&>(bunch, *this);
+
+        for (size_t i=1; i<bunch.get_nslices() + 1; i++)
+        {
+            poisson.fastgather<Bunch&>(bunch, i);
+            poisson.computePotential<Bunch&>(bunch, i);
+            poisson.compute_field<Bunch&>(bunch, i);
+
+            poisson.fastgather<Cloud&>(*this, i);
+            poisson.computePotential<Cloud&>(*this, i);
+            poisson.compute_field<Cloud&>(*this, i);
+
+            poisson.parallelscatter<Bunch&, Cloud&>(bunch, *this, i);
+
+            this->push(bunch, i);
+        }
+        t0 += omp_get_wtime() - t1;
+        std::cout << "\n  Time elapsed: " << std::scientific
+                  << t0 << " s" << std::endl;
+    }
+}
+
 
 class Ghost(object):
 
     def set_beam_physics(self): pass
 
     def set_beam_numerics(self): pass
+
 
 def bunch_matched_and_sliced(n_macroparticles, n_particles, charge, energy, mass,
                              epsn_x, epsn_y, ltm, bunch_length, bucket, matching,
