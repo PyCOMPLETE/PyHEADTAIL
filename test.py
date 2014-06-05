6d59e014b487fa973bfb5f4b419c1ee21346d9e4
Adrian Oeftiger <oeftiger@cern.ch>
2014-06-05 13:38:18 +0200

    in the process of synchronising potential and track methods of the Kick class

---
+++ test.py
@@ -0,0 +1,38 @@
+n_turns = 10001
+from trackers.longitudinal_tracker import * 
+from beams.beams import *
+a = RFSystems(np.pi * 50, [1, 2], [8000,6000], [0, np.pi], [4.05**-2], m_p * 0.84 * 2 * c * 1e-6)
+beam = Particles.as_gaussian(1000, e, 2, 1.e11, m_p, 0, 54.6408, 2, 0, 54.5054, 2, 15, 0.001)
+import matplotlib as mpl
+n = 1
+x = np.arange(n * -100, n * 100, 0.1)
+y = np.arange(n * -0.005, n * 0.005, 0.00001)
+X, Y = np.meshgrid(x,y)
+Z = a.hamiltonian(X, Y, beam)
+
+extent=[x.min(), x.max(), y.min(), y.max()]
+
+import sys
+import matplotlib.pyplot as plt
+plt.ion()
+plt.figure()
+
+for i in xrange(n_turns):
+	a.track(beam)
+	if i%50 is not 0:
+		continue
+	sys.stdout.write("\r %d" % i)
+	sys.stdout.flush()
+#	plt.clf()
+	plt.gca()
+	#im = plt.imshow(Z, interpolation="bilinear", origin="lower", vmin=Z.min(), vmax=Z.max(), extent=[x.min(), x.max(), y.min(), y.max()], aspect="auto")
+	CS = plt.contour(X, Y, Z, 25)#, colors="black")
+	plt.clabel(CS, fontsize=9, inline=1)
+	#ZZ, DP = np.meshgrid(beam.z, beam.dp)
+	HAM = a.hamiltonian(beam.z, beam.dp, beam)
+	plt.scatter(beam.z, beam.dp, alpha=0.8, linewidth=0.1, c=HAM, marker=".")
+	plt.xlim(x.min(), x.max())
+	plt.ylim(y.min(), y.max())
+	plt.draw()
+
+raw_input("\nPress Enter to continue...")
--- trackers/longitudinal_tracker.py
+++ trackers/longitudinal_tracker.py
@@ -70,30 +70,48 @@ class Drift(LongitudinalMap):
     the (separable) Hamiltonian derived by dp (defined by (p - p0) / p0).
 
     self.length is the drift length,
-    self.beta_factor is the change ratio of \\beta_{n+1} / \\beta_n
-    which can often be neglected (and be set to one). [Otherwise it may
-    continuously be adapted by the user according to Kick.p_increment.]
+    self.shrinkage_p_increment being non-zero includes the shrinking 
+    ratio \\beta_{n+1} / \\beta_n (see MacLachlan 1989 in FN-529),
+    it is usually neglected. [Otherwise it may continuously be 
+    adapted by the user according to the total momentum increment.]
+    If it is not neglected, the beta factor ratio would yield
+    (\\beta + \\Delta \\beta) / \\beta = 
+                        = 1 - \\Delta \\gamma / (\\beta^2 * \\gamma^2)
+    resp.               = 1 - p_increment / (\\gamma^3 * p0)
+    since p_increment = \\gamma * m * c / \\beta * \\Delta gamma .
     """
 
-    def __init__(self, alpha_array, length, beta_factor = 1):
+    def __init__(self, alpha_array, length, shrinkage_p_increment=0):
         super(Drift, self).__init__(alpha_array)
         self.length = length
-        self.beta_factor = beta_factor
+        self.shrinkage_p_increment = shrinkage_p_increment
 
     def track(self, beam):
-        beam.z = (self.beta_factor * beam.z - 
-            self.eta(beam.dp, beam) * beam.dp * self.length)
+        beta_ratio = 1 - self.shrinkage_p_increment / (beam.gamma**3 * beam.p0)
+        beam.z = (beta_ratio * beam.z -
+                  self.eta(beam.dp, beam) * beam.dp * self.length)
 
 class Kick(LongitudinalMap):
-    """The Kick class represents the kick by a single RF element in a ring!
-    The kick (i.e. Delta dp) of the particle's dp coordinate is given by
-    the (separable) Hamiltonian derived by z, i.e. the force.
+    """The Kick class represents the kick by a single RF element 
+    in a ring! The kick (i.e. Delta dp) of the particle's dp 
+    coordinate is given by the (separable) Hamiltonian derived 
+    by z, i.e. the force.
 
-    self.p_increment is the momentum step per turn of the synchronous particle,
-        it can be continuously adjusted to reflect different slopes 
-        in the dipole magnet strength ramp.
+    self.p_increment is the momentum step per turn of the 
+    synchronous particle, it can be continuously adjusted externally 
+    by the user to reflect different slopes in the dipole field ramp.
 
-    self.phi_offset reflects an offset of the cavity's reference system."""
+    self.phi_offset reflects an offset of the cavity's reference system,
+    this can be tweaked externally by the user for simulating RF system
+    ripple and the like.
+    (self._phi_acceleration adds to the offset as well but should
+    be used internally in the module (e.g. by RFSystems) for 
+    acceleration purposes. It may be used for synchronisation with the
+    momentum updating by self.p_increment via self.calc_phi_0(beam),
+    thus readjusting the zero-crossing of this sinosoidal kick.
+    This requires a convention how to mutually displace the Kick
+    phases to each other w.r.t. to their contribution to acceleration.)
+    """
 
     def __init__(self, alpha_array, circumference, harmonic, voltage, 
                  phi_offset=0, p_increment=0):
@@ -103,15 +121,15 @@ class Kick(LongitudinalMap):
         self.voltage = voltage
         self.phi_offset = phi_offset
         self.p_increment = p_increment
+        self._phi_acceleration = 0
 
     def track(self, beam):
         sgn_eta = np.sign(self.eta(0, beam))
         amplitude = sgn_eta * e * self.voltage / (beam.beta * c)
         theta = (2 * np.pi / self.circumference) * beam.z
-        phi = self.harmonic * theta + self.phi_offset
-
+        phi = self.harmonic * theta + self.phi_offset + self._phi_acceleration
         beam.delta_p += amplitude * sin(phi) - self.p_increment
-        beam.p0  += self.p_increment
+        beam.p0 += self.p_increment
 
     def Qs(self, beam):
         '''
@@ -119,22 +137,27 @@ class Kick(LongitudinalMap):
 
         .. math::
         H = -1 / 2 * eta * beta * c * delta ** 2
-           + e * V / (p0 * 2 * np.pi * h) * 
+            + e * V / (p0 * 2 * np.pi * h) * 
             * (np.cos(phi) - np.cos(dphi) + (phi - dphi) * np.sin(dphi))
-        ASSUMPTION: this is the only Kick instance in the ring layout 
-            (i.e. only a single harmonic RF system)!
+        NOTE: This function only returns the synchroton tune effectuated
+        by this single Kick instance, any contribution from other Kick
+        objects is not taken into account! (I.e. in general, this
+        calculated value is wrong for multi-harmonic RF systems.)
         '''
-        Qs = np.sqrt(e * self.voltage * np.abs(self.eta(0, beam)) * \
+        Qs = np.sqrt(e * self.voltage * np.abs(self.eta(0, beam)) *
                     self.harmonic / (2 * np.pi * beam.p0 * beam.beta * c))
         return Qs
 
     def calc_phi_0(self, beam):
-        """The synchronous phase calculated from the momentum increase per turn.
-        It includes the jump in the e.o.m. (via sign(eta)) at transition energy:
+        """The phase deviation from the unaccelerated case
+        calculated via the momentum step self.p_increment
+        per turn. It includes the jump in the e.o.m. 
+        (via sign(eta)) at transition energy:
             gamma < gamma_transition <==> phi_0 ~ pi
             gamma > gamma_transition <==> phi_0 ~ 0
-        ASSUMPTION: this is the only Kick instance adding to acceleration
-        (i.e. technically the only Kick instance with self.p_increment != 0)!"""
+        In the case of only one Kick element in the ring, this phase 
+        deviation coincides with the synchronous phase!
+        """
         if self.p_increment == 0 and self.voltage == 0:
             return 0
         deltaE  = self.p_increment * c * beam.beta / beam.gamma
@@ -147,9 +170,10 @@ class Kick(LongitudinalMap):
         if phi_0 is None:
             phi_0 = self.calc_phi_0(beam)
         theta = (2 * np.pi / self.circumference) * z
-        phi = self.harmonic * theta + self.phi_offset
+        phi = self.harmonic * theta + self.phi_offset + self._phi_acceleration
         amplitude = -e * self.voltage / (beam.p0 * 2 * np.pi * self.harmonic)
-        modulation = cos(phi) - cos(phi_0) + (phi - phi_0) * sin(phi_0)
+        modulation = (cos(-phi) - cos(phi_0)
+                      + (-phi - phi_0) * sin(phi_0))
         return amplitude * modulation
 
 
@@ -172,25 +196,31 @@ class LongitudinalOneTurnMap(LongitudinalMap):
     @abstractmethod
     def track(self, beam):
         """Contract: advances the longitudinal coordinates 
-        of the beam over a full turn."""
+        of the beam over a full turn / circumference."""
         pass
 
 class RFSystems(LongitudinalOneTurnMap):
     """
-        With one RFSystems object in the ring layout (with all kicks applied 
-        at the same longitudinal position), the longitudinal separatrix function 
-        is exact and makes a valid local statement about stability!
+        With one RFSystems object in the ring layout (with all Kick
+        objects located at the same longitudinal position), the 
+        longitudinal separatrix function is exact and makes a valid 
+        local statement about stability!
     """
 
     def __init__(self, circumference, harmonic_list, voltage_list, 
-                        phi_offset_list, alpha_array, p_increment=0):
-        """The first entry in harmonic_list, voltage_list and phi_offset_list
-        defines the parameters for the one accelerating Kick object 
-        (i.e. the accelerating RF system).
+                 phi_offset_list, alpha_array, p_increment=0, shrinking=False):
+        """The first entry in harmonic_list, voltage_list and 
+        phi_offset_list defines the parameters for the one 
+        accelerating Kick object (i.e. the accelerating RF system).
+        For several accelerating Kick objects one would have to
+        extend this class and settle for the relative phases
+        between the Kick objects! (For one accelerating Kick object,
+        all the other Kick objects' zero crossings are displaced by
+        the negative phase shift induced by the accelerating Kick.)
 
         The length of the momentum compaction factor array alpha_array
         defines the order of the slippage factor expansion. 
-        See the LongitudinalMap class for further details.
+        (See the LongitudinalMap class for further details.)
 
         RFSystems comprises a half the circumference drift, 
         then all the kicks by the RF Systems in one location, 
@@ -201,14 +231,24 @@ class RFSystems(LongitudinalOneTurnMap):
         a symmetric way (otherwise phase space should be tilted
         at the entrance or exit of the cavity / kick location!).
 
-        self.p_increment is the momentum step per turn of the synchronous 
-        particle, it can be continuously adjusted to reflect different slopes 
-        in the dipole magnet strength ramp.
-        See the Kick class for further details.
-        self.kicks
-        self.elements
-        self.fundamental_kick
-        self.accelerating_kick"""
+        The boolean parameter shrinking determines whether the
+        shrinkage ratio \\beta_{n+1} / \\beta_n should be taken 
+        into account during the second Drift.
+        (See the Drift class for further details.)
+
+        - self.p_increment is the momentum step per turn of the 
+        synchronous particle, it can be continuously adjusted to 
+        reflect different slopes in the dipole magnet strength ramp.
+        (See the Kick class for further details.)
+        - self.kicks is a list of the Kick objects (defined by the 
+        respective lists in the constructor)
+        - self.accelerating_kick returns the first Kick object in
+        self.kicks which carries the only p_increment != 0
+        - self.elements is comprised of a half turn Drift, self.kicks,
+        and another half turn Drift
+        - self.fundamental_kick returns the Kick object with the lowest
+        harmonic of the revolution frequency
+        """
 
         super(RFSystems, self).__init__(alpha_array, circumference)
 
@@ -216,12 +256,13 @@ class RFSystems(LongitudinalOneTurnMap):
             print ("Warning: parameter lists for RFSystems " +
                                         "do not have the same length!")
 
+        self._shrinking = shrinking
         self.kicks = []
         for h, V, dphi in zip(harmonic_list, voltage_list, phi_offset_list):
             kick = Kick(alpha_array, self.circumference, h, V, dphi)
             self.kicks.append(kick)
         self.elements = ( [Drift(alpha_array, self.circumference / 2)] 
-                        + [self.kicks]
+                        + self.kicks
                         + [Drift(alpha_array, self.circumference / 2)]
                         )
         self.accelerating_kick = self.kicks[0]
@@ -230,7 +271,7 @@ class RFSystems(LongitudinalOneTurnMap):
 
     def track(self, beam):
         if self.p_increment:
-            betagamma_old   = beam.betagamma
+            betagamma_old = beam.betagamma
         for longMap in self.elements:
             longMap.track(beam)
         if self.p_increment:
@@ -251,16 +292,18 @@ class RFSystems(LongitudinalOneTurnMap):
     @p_increment.setter
     def p_increment(self, value):
         self.accelerating_kick.p_increment = value
-    
+        if self._shrinking:
+            self.elements[-1].shrinkage_p_increment = value
+
 
     def potential(self, z, beam):
         """the potential well of the rf system"""
         phi_0 = self.accelerating_kick.calc_phi_0(beam)
         h1 = self.accelerating_kick.harmonic
         def fetch_potential(kick):
-            phi_0_i = -kick.harmonic / h1 * phi_0
+            phi_acc_individual = -kick.harmonic / h1 * phi_0
             if kick is not self.accelerating_kick:
-                kick.phi_offset = phi_0_i + np.pi
+                kick._phi_acceleration = phi_acc_individual
             return kick.potential(z, beam)
         potential_list = map(fetch_potential, self.kicks)
         return sum(potential_list)
