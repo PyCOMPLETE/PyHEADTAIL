8394f86d7ba5a27c18f5d2c6f526db730d03fc76
michuschenk <michuschenk@gmail.com>
2014-07-01 11:21:14 +0200

    Adds first implementation of RFQ as a detuning element. Not yet tested.
     - 3 different parameterizations for detune method are implemented.
     - Longitudinal detuning is missing.
     - Looking for a more elegant way to select parameterization of detune method.

--- trackers/detuners.py
+++ trackers/detuners.py
@@ -10,22 +10,24 @@ from scipy.constants import e, c
 import numpy as np
 
 
+cos = np.cos
+
 class Detuner(object):
     """
+    
     """
-
     __metaclass__ = ABCMeta
 
     @abstractmethod
     def detune(self, beam):
         """
-        Calculates the detune of the corresponding detuner element.
+        Calculates the detune caused by the corresponding detuner element.
         """
         pass
 
 
 """
-Some examples of commonly used detuner elements. To be extended.
+Some commonly used detuner elements. To be extended.
 """
 class SextupoleSegment(Detuner):
 
@@ -70,11 +72,63 @@ class OctupoleSegment(Detuner):
         
 class RFQSegment(Detuner):
 
-    pass
+    def __init__(self, dapp_xz, dapp_yz, omega_RF, detune_function_to_use):
+        
+        self.dapp_xz = dapp_xz
+        self.dapp_yz = dapp_yz
+        self.omega_RF = omega_RF
+
+        # self.detune_function_to_use = detune_function_to_use
+        self.phi_0_RF = 0
+
+        # SELECT DETUNING FUNCTION
+        if detune_function_to_use == 'general':
+            self.detune = self.detune_general
+        elif detune_function_to_use == 'on_crest':
+            self.detune = self.detune_on_crest
+        elif detune_function_to_use == 'off_crest':
+            self.detune = self.detune_off_crest
+        else:
+            print 'Detuning method not available.'
 
 
+    def detune(self):
+        pass
+
+
+    def detune_general(self, beam):
+        """
+        Most general detuning function.
+        """
+        # HERE WE WILL NEED ACCESS TO PHI_0_RF
+        dphi_x = self.dapp_xz / (beam.dp * beam.p0 + beam.p0) * cos(self.omega_RF * beam.z / (beam.beta * c) + self.phi_0_RF)
+        dphi_y = self.dapp_yz / (beam.dp * beam.p0 + beam.p0) * cos(self.omega_RF * beam.z / (beam.beta * c) + self.phi_0_RF)
+        
+        return dphi_x, dphi_y
+
+            
+    def detune_on_crest(self, beam):
+        """
+        Approximative detuning function assuming beam (center) entering on crest.
+        """
+        dphi_x = self.dapp_xz / (beam.dp * beam.p0 + beam.p0) * (1. - 1./2. * (beam.z * self.omega_RF / (beam.beta * c))**2)
+        dphi_y = self.dapp_yz / (beam.dp * beam.p0 + beam.p0) * (1. - 1./2. * (beam.z * self.omega_RF / (beam.beta * c))**2)
+
+        return dphi_x, dphi_y
+
+    
+    def detune_off_crest(self, beam):
+        """
+        Approximative detuning function assuming beam (center) entering off crest.
+        """
+        dphi_x = self.dapp_xz / (beam.dp * beam.p0 + beam.p0) * beam.z * self.omega_RF / (beam.beta * c)
+        dphi_y = self.dapp_yz / (beam.dp * beam.p0 + beam.p0) * beam.z * self.omega_RF / (beam.beta * c)
+
+        return dphi_x, dphi_y
+        
+        
 """
-Collection classes for each class of detuner. These are the classes instantiated explicitly by a user.
+Collection classes for each class of detuner. These are the classes instantiated explicitly by the user.
 They use 1-turn integrated values as input and instantiate detuners for each segment in 's' with a
 detuning proportional to the segment length.
 """
@@ -82,7 +136,7 @@ class Octupole(object):
 
     def __init__(self, s, beta_x, beta_y, app_x, app_y, app_xy):
 
-        self.s      = s
+        self.s = s
         self.beta_x = beta_x
         self.beta_y = beta_y
 
@@ -101,7 +155,7 @@ class Octupole(object):
         'THE LHC TRANSVERSE COUPLED-BUNCH INSTABILITY' (EPFL PhD Thesis), N. Mounet, 2012
         from LHC octupole currents i_focusing, i_defocusing [A].
 
-        Measurement values (hard-coded) were obtained before LS1.
+        Measurement values (hard-coded numbers) were obtained before LS1.
         """
         i_max = 550.  # [A]
         E_max = 7000. # [GeV]
@@ -145,7 +199,7 @@ class Sextupole(object):
 
     def __init__(self, s, Qp_x, Qp_y):
 
-        self.s      = s
+        self.s = s
 
         scale_to_segment = np.diff(s) / s[-1]
         self.dQp_x = Qp_x * scale_to_segment
@@ -178,4 +232,46 @@ class Sextupole(object):
 
 class RFQ(object):
     
-    pass
+    def __init__(self, s, beta_x, beta_y, voltage_RF, omega_RF, detune_function_to_use):
+
+        self.s = s
+        self.omega_RF = omega_RF
+
+        self.detune_function_to_use = detune_function_to_use
+        
+        # Calculate matrix elements following 'Radio frequency quadrupole for Landau damping in accelerators',
+        # A. Grudiev, 2014.
+        # k2  = 2. * voltage_RF * e / ((beam.dp * beam.p0 + beam.p0) * omega_RF)
+        # app_xz and app_yz are constant pre-factors valid for any parameterization of detuning function.
+        app = voltage_RF / omega_RF * e / (2. * np.pi)
+        app_xz = -beta_x * app
+        app_yz = beta_y * app
+        
+        scale_to_segment = np.diff(s) / s[-1]
+        self.dapp_xz = app_xz * scale_to_segment
+        self.dapp_yz = app_yz * scale_to_segment
+        
+        self._generate_segment_detuners()
+        
+        
+    def _generate_segment_detuners(self):
+
+        segment_detuners = []
+
+        n_segments = len(self.s) - 1
+        for seg in range(n_segments):
+            segment_detuner = RFQSegment(self.dapp_xz[seg], self.dapp_yz[seg], self.omega_RF, self.detune_function_to_use)
+            segment_detuners.append(segment_detuner)
+
+        self.segment_detuners = segment_detuners
+
+        
+    def __len__(self):
+
+        return len(self.segment_detuners)
+
+    
+    def __getitem__(self, key):
+
+        return self.segment_detuners[key]
+
