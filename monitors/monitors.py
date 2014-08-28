7ee7ba322e1345a5043cbd4a5730632ac40aa7d5
michuschenk <michuschenk@gmail.com>
2014-08-28 11:57:07 +0200

    Slims down slice monitor. Adds buffer to it.
     - Particle monitor yet to be cleaned.

--- monitors/monitors.py
+++ monitors/monitors.py
@@ -26,22 +26,13 @@ class BunchMonitor(Monitor):
                                   'n_macroparticles' ]
         self.n_steps  = n_steps
         self.i_steps  = 0
-	self.filename = filename
+        self.filename = filename
 
         self.buffer_size = 2048
-        self.buffer = np.zeros((len(self.stats_quantities), self.buffer_size))
         self.write_buffer_to_file_every = 1024
+        self.buffer = np.zeros((len(self.stats_quantities), self.buffer_size))
 
-        try:
-            self.h5file = hp.File(filename + '.h5', 'w')
-            if dictionary:
-                for key in dictionary:
-                    self.h5file.attrs[key] = dictionary[key]
-            self._create_file_structure()            
-	    self.h5file.close()
-        except:
-            print 'Creation of bunch monitor file failed.'
-            sys.exit(-1)
+        self._create_file_structure(dictionary)
 
 
     def dump(self, bunch):
@@ -53,13 +44,24 @@ class BunchMonitor(Monitor):
         self.i_steps += 1
 
 
-    def _create_file_structure(self):
+    def _create_file_structure(self, dictionary):
 
-        self.h5file.create_group('Bunch')
+        try:
+            h5file = hp.File(self.filename + '.h5', 'w')
 
-        h5group = self.h5file['Bunch']
-        for stats in sorted(self.stats_quantities):
-            h5group.create_dataset(stats, shape=(self.n_steps,), compression='gzip', compression_opts=9)
+            if dictionary:
+                for key in dictionary:
+                    h5file.attrs[key] = dictionary[key]
+
+            h5file.create_group('Bunch')
+            h5group = h5file['Bunch']
+            for stats in sorted(self.stats_quantities):
+                h5group.create_dataset(stats, shape=(self.n_steps,), compression='gzip', compression_opts=9)
+
+            h5file.close()
+        except:
+            print 'Creation of bunch monitor file failed.'
+            sys.exit(-1)
             
 
     def _write_data_to_buffer(self, bunch):
@@ -82,12 +84,12 @@ class BunchMonitor(Monitor):
         up_pos_in_file      = self.i_steps + 1
 
         try:       
- 	    self.h5file = hp.File(self.filename + '.h5', 'a')
-            h5group = self.h5file['Bunch']
+            h5file  = hp.File(self.filename + '.h5', 'a')
+            h5group = h5file['Bunch']
             for k, stats in enumerate(sorted(self.stats_quantities)):
                 h5group[stats][low_pos_in_file:up_pos_in_file] = self.buffer[k, low_pos_in_buffer:]
-
-	    self.h5file.close()
+            h5file.close()
+            
         except:
             print 'Bunch monitor file is not accessible.'
 
@@ -95,95 +97,88 @@ class BunchMonitor(Monitor):
 class SliceMonitor(Monitor):
 
     def __init__(self, filename, n_steps, dictionary=None, slices=None):
+        
+        self.stats_quantities = [ 'mean_x', 'mean_xp', 'mean_y', 'mean_yp', 'mean_z', 'mean_dp',
+                                  'sigma_x', 'sigma_y', 'sigma_z', 'sigma_dp', 'epsn_x', 'epsn_y', 'epsn_z',
+                                  'n_macroparticles' ]
+        self.n_steps  = n_steps
+        self.i_steps  = 0
+        self.filename = filename
+        self.slices   = slices
 
-        self.n_steps = n_steps
-        self.slices  = slices
+        self.buffer_size = 2048
+        self.write_buffer_to_file_every = 1024
+        self.buffer_bunch = np.zeros((len(self.stats_quantities), self.buffer_size))
+        self.buffer_slice = np.zeros((len(self.stats_quantities), self.slices.n_slices, self.buffer_size))
 
-        self.h5file = hp.File(filename + '.h5', 'w')
-        if dictionary:
-            for key in dictionary:
-                self.h5file.attrs[key] = dictionary[key]
-
-        self.h5file.create_group('Bunch')
-        self.h5file.create_group('Slices')
-        self.h5file.flush()
+        self._create_file_structure(dictionary)
 
 
     def dump(self, bunch):
 
-        if not self.slices:
-            self.slices = bunch.slices
+        self._write_data_to_buffer(bunch)
+        if (self.i_steps + 1) % self.write_buffer_to_file_every == 0 or (self.i_steps + 1) == self.n_steps:
+            self._write_buffer_to_file()
 
+        self.i_steps += 1
+
+
+    def _create_file_structure(self, dictionary):
+                
         try:
-            self.i_steps += 1
-            self._write_bunch_data(bunch)
-            self._write_slice_data(bunch)
-        except AttributeError:
-            self.i_steps = 0
-            self._create_data(self.h5file['Bunch'],  (self.n_steps,))
-            self._create_data(self.h5file['Slices'], (self.slices.n_slices, self.n_steps))
-            self._write_bunch_data(bunch)
-            self._write_slice_data(bunch)
+            h5file = hp.File(self.filename + '.h5', 'w')
 
-        self.h5file.flush()
+            if dictionary:
+                for key in dictionary:
+                    h5file.attrs[key] = dictionary[key]
 
-
-    def _create_data(self, h5group, dims):
-
-        h5group.create_dataset("mean_x",   dims, compression="gzip", compression_opts=9)
-        h5group.create_dataset("mean_xp",  dims, compression="gzip", compression_opts=9)
-        h5group.create_dataset("mean_y",   dims, compression="gzip", compression_opts=9)
-        h5group.create_dataset("mean_yp",  dims, compression="gzip", compression_opts=9)
-        h5group.create_dataset("mean_z",   dims, compression="gzip", compression_opts=9)
-        h5group.create_dataset("mean_dp",  dims, compression="gzip", compression_opts=9)
-        h5group.create_dataset("sigma_x",  dims, compression="gzip", compression_opts=9)
-        h5group.create_dataset("sigma_y",  dims, compression="gzip", compression_opts=9)
-        h5group.create_dataset("sigma_z",  dims, compression="gzip", compression_opts=9)
-        h5group.create_dataset("sigma_dp", dims, compression="gzip", compression_opts=9)
-        h5group.create_dataset("epsn_x",   dims, compression="gzip", compression_opts=9)
-        h5group.create_dataset("epsn_y",   dims, compression="gzip", compression_opts=9)
-        h5group.create_dataset("epsn_z",   dims, compression="gzip", compression_opts=9)
-        h5group.create_dataset("n_macroparticles", dims, compression="gzip", compression_opts=9)
+            h5file.create_group('Bunch')
+            h5file.create_group('Slices')        
+            h5group_bunch = h5file['Bunch']
+            h5group_slice = h5file['Slices']
+            for stats in sorted(self.stats_quantities):
+                h5group_bunch.create_dataset(stats, shape=(self.n_steps,), compression='gzip', compression_opts=9)
+                h5group_slice.create_dataset(stats, shape=(self.slices.n_slices, self.n_steps), compression='gzip', compression_opts=9)
+            h5file.close()
+            
+        except:
+            print 'Creation of slice monitor file failed.'
+            sys.exit(-1)
 
 
-    def _write_bunch_data(self, bunch):
+    def _write_data_to_buffer(self, bunch):
 
-        h5group = self.h5file['Bunch']
+        for k, stats in enumerate(sorted(self.stats_quantities)):
+            evaluate_stats_bunch = getattr(bunch, stats)
+            evaluate_stats_slice = getattr(self.slices, stats)
+            try:
+                self.buffer_bunch[k,0] = evaluate_stats_bunch()
+                self.buffer_slice[k,:,0] = evaluate_stats_slice(bunch)
+            except TypeError:
+                self.buffer_bunch[k,0] = evaluate_stats_bunch
+                self.buffer_slice[k,:,0] = evaluate_stats_slice
+                
+        self.buffer_bunch = np.roll(self.buffer_bunch, shift=-1, axis=1)
+        self.buffer_slice = np.roll(self.buffer_slice, shift=-1, axis=2)
+        
 
-        h5group["mean_x"][self.i_steps]   = bunch.mean_x()
-        h5group["mean_xp"][self.i_steps]  = bunch.mean_xp()
-        h5group["mean_y"][self.i_steps]   = bunch.mean_y()
-        h5group["mean_yp"][self.i_steps]  = bunch.mean_yp()
-        h5group["mean_z"][self.i_steps]   = bunch.mean_z()
-        h5group["mean_dp"][self.i_steps]  = bunch.mean_dp()
-        h5group["sigma_x"][self.i_steps]  = bunch.sigma_x()
-        h5group["sigma_y"][self.i_steps]  = bunch.sigma_y()
-        h5group["sigma_z"][self.i_steps]  = bunch.sigma_z()
-        h5group["sigma_dp"][self.i_steps] = bunch.sigma_dp()
-        h5group["epsn_x"][self.i_steps]   = bunch.epsn_x()
-        h5group["epsn_y"][self.i_steps]   = bunch.epsn_y()
-        h5group["epsn_z"][self.i_steps]   = bunch.epsn_z()
-        h5group["n_macroparticles"][self.i_steps] = bunch.n_macroparticles
+    def _write_buffer_to_file(self):
 
+        n_entries_in_buffer = min(self.i_steps+1, self.buffer_size)
+        low_pos_in_buffer   = self.buffer_size - n_entries_in_buffer
+        low_pos_in_file     = self.i_steps + 1 - n_entries_in_buffer
+        up_pos_in_file      = self.i_steps + 1
 
-    def _write_slice_data(self, bunch):
-
-        h5group = self.h5file['Slices']
-
-        h5group["mean_x"][:,self.i_steps]   = self.slices.mean_x(bunch)
-        h5group["mean_xp"][:,self.i_steps]  = self.slices.mean_xp(bunch)
-        h5group["mean_y"][:,self.i_steps]   = self.slices.mean_y(bunch)
-        h5group["mean_yp"][:,self.i_steps]  = self.slices.mean_yp(bunch)
-        h5group["mean_z"][:,self.i_steps]   = self.slices.mean_z(bunch)
-        h5group["mean_dp"][:,self.i_steps]  = self.slices.mean_dp(bunch)
-        h5group["sigma_x"][:,self.i_steps]  = self.slices.sigma_x(bunch)
-        h5group["sigma_y"][:,self.i_steps]  = self.slices.sigma_y(bunch)
-        h5group["sigma_z"][:,self.i_steps]  = self.slices.sigma_z(bunch)
-        h5group["sigma_dp"][:,self.i_steps] = self.slices.sigma_dp(bunch)
-        h5group["epsn_x"][:,self.i_steps]   = self.slices.epsn_x(bunch)
-        h5group["epsn_y"][:,self.i_steps]   = self.slices.epsn_y(bunch)
-        h5group["epsn_z"][:,self.i_steps]   = self.slices.epsn_z(bunch)
-        h5group["n_macroparticles"][:,self.i_steps] = self.slices.n_macroparticles
+        try:       
+            h5file = hp.File(self.filename + '.h5', 'a')
+            h5group_bunch = h5file['Bunch']
+            h5group_slice = h5file['Slices']
+            for k, stats in enumerate(sorted(self.stats_quantities)):
+                h5group_bunch[stats][low_pos_in_file:up_pos_in_file]   = self.buffer_bunch[k, low_pos_in_buffer:]
+                h5group_slice[stats][:,low_pos_in_file:up_pos_in_file] = self.buffer_slice[k,:,low_pos_in_buffer:]
+            h5file.close()
+        except:
+            print 'Slices monitor file is not accessible.'
 
 
 class ParticleMonitor(Monitor):
