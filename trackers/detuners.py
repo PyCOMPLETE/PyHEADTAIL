b4ef365e3ff9968345b93cc11a097c2a69644323
michuschenk <michuschenk@gmail.com>
2014-10-23 09:29:09 +0200

    Clean unused code after merge with oeftiger-min

--- trackers/detuners.py
+++ trackers/detuners.py
@@ -9,10 +9,10 @@ object for each type of detuning effect present in the accelerator. It
 provides a description of the detuning along the full circumference. The
 accelerator is divided into segments (1 or more) and the
 DetunerCollection can create and store a SegmentDetuner object of the
-given type of detuning for each of the segments that are defined by the
-user. A SegmentDetuner object has a detune(beam) method that defines how
-the tune of each particle in the beam is changed according to the
-formula describing the effect.
+given type of detuning for each of these segments. A SegmentDetuner
+object has a detune(beam) method that defines how the phase advance of
+each particle in the beam is changed according to the formula describing
+the effect.
 
 @author Kevin Li, Michael Schenk, Adrian Oeftiger
 @date 23. June 2014
@@ -29,14 +29,12 @@ from abc import ABCMeta, abstractmethod
 
 
 class SegmentDetuner(object):
-    """
-    Abstract base class for detuning elements and effects defined only
-    for a segment of the accelerator ring (NB. The segment can also be
-    given by the full circumference).
+    """ Abstract base class for detuning elements and effects defined
+    only for a segment of the accelerator ring (NB. The segment can also
+    be given by the full circumference).
     Every detuner element/effect inheriting from this class must
-    implement the detune(beam) method to describe the change in tune
-    for each particle in the beam.
-    """
+    implement the detune(beam) method to describe the change in phase
+    advance for each particle of the beam. """
     __metaclass__ = ABCMeta
 
     @abstractmethod
@@ -45,22 +43,21 @@ class SegmentDetuner(object):
 
 
 class ChromaticitySegment(SegmentDetuner):
-    """
-    Detuning object for a segment of the accelerator ring to describe
-    the detuning introduced by chromaticity effects.
+    """ Detuning object for a segment of the accelerator ring to
+    describe the detuning introduced by chromaticity effects.
+    TODO Implement second and third order chromaticity effects. """
 
-    TO DO:
-    Implement second and third order chromaticity effects.
-    """
     def __init__(self, dQp_x, dQp_y):
+        """ Return an instance of a ChromaticitySegment. The dQp_{x,y}
+        denote the first order chromaticity coefficients scaled to the
+        segment length. """
         self.dQp_x = dQp_x
         self.dQp_y = dQp_y
 
     def detune(self, beam):
-        """
-        Calculates for every particle the change in phase advance
-        (detuning) dQ_x,y  caused by first-order chromaticity effects.
-        """
+        """ Calculate for every particle the change in phase advance
+        (detuning) dQ_{x,y}  caused by first order chromaticity
+        effects. """
         dQ_x = self.dQp_x * beam.dp
         dQ_y = self.dQp_y * beam.dp
 
@@ -68,17 +65,17 @@ class ChromaticitySegment(SegmentDetuner):
 
 
 class AmplitudeDetuningSegment(SegmentDetuner):
-    """
-    Detuning object for a segment of the accelerator ring to describe
-    amplitude detuning (introduced by octupoles).
-    """
+    """ Detuning object for a segment of the accelerator ring to
+    describe amplitude detuning (introduced by octupoles). """
+
     def __init__(self, dapp_x, dapp_y, dapp_xy, beta_x, beta_y):
-        """
-        Note: Beta_x and beta_y are only used to correctly calculate the
-        transverse action J_x and J_y. Although they have an influence
-        on the strength of detuning, they have no actual effect on the
-        strength of the octupoles (dapp_x, dapp_y, dapp_xy).
-        """
+        """ Return an instance of an AmplitudeDetuningSegment by passing
+        the coefficients of detuning strength dapp_x, dapp_y, dapp_xy
+        (scaled to the segment length. NOT normalized to Beam.p0 yet).
+        Note that beta_{x,y} are only used to correctly calculate the
+        transverse actions J_{x,y}. Although they have an influence on
+        the strength of detuning, they have no actual effect on the
+        strength of the octupoles (dapp_x, dapp_y, dapp_xy). """
         self.beta_x = beta_x
         self.beta_y = beta_y
 
@@ -87,14 +84,12 @@ class AmplitudeDetuningSegment(SegmentDetuner):
         self.dapp_xy = dapp_xy
 
     def detune(self, beam):
-        """
-        Linear amplitude detuning formula, usually used for detuning
+        """ Linear amplitude detuning formula, usually used for detuning
         introduced by octupoles. The normalization of dapp_x, dapp_y,
         dapp_xy to the reference momentum is done here (compare
         documentation of AmplitudeDetuning class).
         J_x and J_y resp. denote the horizontal and vertical action of
-        a specific particle.
-        """
+        a specific particle. """
         Jx = (beam.x**2 + (self.beta_x * beam.xp)** 2) / (2. * self.beta_x)
         Jy = (beam.y**2 + (self.beta_y * beam.yp)** 2) / (2. * self.beta_y)
 
@@ -105,12 +100,11 @@ class AmplitudeDetuningSegment(SegmentDetuner):
 
 
 class DetunerCollection(object):
-    """
-    Base class for a collection of SegmentDetuner objects (see above).
-    A detuner collection object defines the detuning for one complete
-    turn around the accelerator ring for the given detuning element.
-    Hence, the strength of detuning must be specified by the user as
-    integrated over one turn.
+    """ Abstract base class for a collection of SegmentDetuner objects
+    (see above). A detuner collection object defines the detuning for
+    one complete turn around the accelerator ring for the given
+    detuning element. Hence, the strength of detuning must be specified
+    by the user as integrated over one turn.
     The accelerator ring is divided into a number of segments (often
     there is just 1). To apply the detuning segment-wise, a
     SegmentDetuner object is instantiated for each of the accelerator
@@ -123,25 +117,24 @@ class DetunerCollection(object):
     SegmentDetuner objects are stored in the segment_detuners list (in
     order of segments along the ring) within the DetunerCollection.
 
-    The individual SegmentDetuner objects stored by a DetunerCollection
-    can be accessed via square brackets [i] with i the index of the
-    segment.
-    """
+    Since the DetunerCollection is implemented as a sequence, the
+    individual SegmentDetuner objects stored by a DetunerCollection can
+    be accessed via square brackets [i] where i is the index of the
+    segment. """
+
     __metaclass__ = ABCMeta
 
     @abstractmethod
     def generate_segment_detuner(self, segment_length, **kwargs):
-        """
-        Instantiates a SegmentDetuner of the given type for a segment
-        of the accelerator ring. Note that the segment_length is given
-        as a relative value, i.e. in units of accelerator circumference.
-        It scales the one turn value for the detuning strength
-        proportionally to the segment length. The method is called by
-        the TransverseMap object which manages the creation of a detuner
-        for every defined segment. The kwargs are used to e.g. pass the
-        beta functions from the TransverseMap where necessary (e.g. for
-        AmplitudeDetuning).
-        """
+        """ Instantiates a SegmentDetuner of the given type for a
+        segment of the accelerator ring. Note that the segment_length
+        is given as a relative value, i.e. in units of accelerator
+        circumference. It scales the one turn value for the detuning
+        strength proportionally to the segment length. The method is
+        called by the TransverseMap object which manages the creation
+        of a detuner for every defined segment. The kwargs are used to
+        e.g. pass the beta functions from the TransverseMap where
+        necessary (e.g. for AmplitudeDetuning). """
         pass
 
     def __len__(self):
@@ -152,17 +145,18 @@ class DetunerCollection(object):
 
 
 class AmplitudeDetuning(DetunerCollection):
-    """
-    Collection class to contain/manage the segment-wise defined
-    amplitude detuning elements (octupoles).
-    """
+    """ Collection class to contain/manage the segment-wise defined
+    amplitude detuning elements (octupoles). They are stored in the
+    self.segment_detuners list. """
+
     def __init__(self, app_x, app_y, app_xy):
-        """
-        Note that the convention used here is such that the coefficients
-        app_x, app_y and app_xy are NOT normalized to the reference
-        momentum beam.p0. The normalization to beam.p0 is done only in
-        the detune(beam) method of the AmplitudeDetuningSegment.
-        """
+        """ Return an instance of the AmplitudeDetuning
+        DetunerCollection class. The coefficients app_x, app_y, app_xy
+        are the detuning strengths (one-turn values). Note that the
+        convention used here is such that they are NOT normalized to
+        the reference momentum Beam.p0. The normalization to Beam.p0
+        is done only in the detune(beam) method of the
+        AmplitudeDetuningSegment. """
         self.app_x  = app_x
         self.app_y  = app_y
         self.app_xy = app_xy
@@ -171,16 +165,15 @@ class AmplitudeDetuning(DetunerCollection):
 
     @classmethod
     def from_octupole_currents_LHC(cls, i_focusing, i_defocusing):
-        """
-        Calculate the constants of proportionality app_x, app_y and
+        """ Calculate the constants of proportionality app_x, app_y and
         app_xy (== app_yx) for the amplitude detuning introduced by the
         LHC octupole magnets (aka. LHC Landau octupoles) from the
         electric currents i_focusing [A] and i_defocusing [A] flowing
         through the magnets. The maximum current is given by
         i_max = +/- 550 [A]. The values app_x, app_y, app_xy  obtained
         from the formulae are proportional to the strength of detuning
-        for one complete turn around the accelerator. Furthermore, they
-        must be converted to SI units.
+        for one complete turn around the accelerator, i.e. one-turn
+        values.
 
         The calculation is based on formulae (3.6) taken from 'The LHC
         transverse coupled-bunch instability' by N. Mounet, EPFL PhD
@@ -194,8 +187,7 @@ class AmplitudeDetuning(DetunerCollection):
 
         More detailed explanations and references on how the formulae
         were obtained are given in the PhD thesis (pg. 85ff) cited
-        above.
-        """
+        above. """
         i_max = 550.  # [A]
         E_max = 7000. # [GeV]
 
@@ -206,6 +198,7 @@ class AmplitudeDetuning(DetunerCollection):
         app_xy = E_max * (-102261. * i_focusing / i_max +
             93331. * i_defocusing / i_max)
 
+        # Convert to SI units.
         convert_to_SI = e / (1.e-9 * c)
         app_x *= convert_to_SI
         app_y *= convert_to_SI
@@ -214,6 +207,15 @@ class AmplitudeDetuning(DetunerCollection):
         return cls(app_x, app_y, app_xy)
 
     def generate_segment_detuner(self, segment_length, **kwargs):
+        """ Instantiates an AmplitudeDetuningSegment for the specified
+        segment of the accelerator ring. Note that the segment_length
+        is given as a relative value, i.e. in units of accelerator
+        circumference. It scales the one-turn values for the detuning
+        strength proportionally to the segment length. The method is
+        called by the TransverseMap object which manages the creation
+        of a detuner for every defined segment. The kwargs are used to
+        pass the beta functions from the TransverseMap at the given
+        segment. """
         dapp_x = self.app_x * segment_length
         dapp_y = self.app_y * segment_length
         dapp_xy = self.app_xy * segment_length
@@ -223,18 +225,28 @@ class AmplitudeDetuning(DetunerCollection):
 
 
 class Chromaticity(DetunerCollection):
-    """
-    Collection class to contain/manage the segment-wise defined elements
-    that introduce detuning as a result of first-order chromaticity
-    effects.
-    """
+    """ Collection class to contain/manage the segment-wise defined
+    elements that introduce detuning as a result of first-order
+    chromaticity effects.  They are stored in the self.segment_detuners
+    list. """
+
     def __init__(self, Qp_x, Qp_y):
+        """ Return an instance of a Chromaticity DetunerCollection
+        class. The Qp_{x,y} are the first order chromaticity
+        coefficients (one-turn values). """
         self.Qp_x = Qp_x
         self.Qp_y = Qp_y
 
         self.segment_detuners = []
 
     def generate_segment_detuner(self, segment_length, **kwargs):
+        """ Instantiates a ChromaticitySegment for the specified
+        segment of the accelerator ring. Note that the segment_length
+        is given as a relative value, i.e. in units of accelerator
+        circumference. It scales the one-turn values for the detuning
+        strength proportionally to the segment length. The method is
+        called by the TransverseMap object which manages the creation
+        of a detuner for every defined segment. """
         dQp_x = self.Qp_x * segment_length
         dQp_y = self.Qp_y * segment_length
 
--- trackers/transverse_tracker.py
+++ trackers/transverse_tracker.py
@@ -1,7 +1,7 @@
 """
 @author Kevin Li, Michael Schenk
 @date 07. January 2014
-@brief Implementation of the transport of transverse phase spaces.
+@brief Description of the transport of transverse phase spaces.
 @copyright CERN
 """
 from __future__ import division
@@ -12,8 +12,7 @@ cos = np.cos
 
 
 class TransverseSegmentMap(object):
-    """
-    Class to transport/track the particles of the beam in the
+    """ Class to transport/track the particles of the beam in the
     transverse plane through an accelerator ring segment defined by its
     boundaries [s0, s1]. To calculate the transverse linear transport
     matrix M that transports each particle's transverse phase space
@@ -25,18 +24,16 @@ class TransverseSegmentMap(object):
     amplitude detuning or chromaticity effects (see trackers.detuners
     module).
 
-    TO DO:
-    Implement dispersion effects, i.e. the change of a particle's
+    TODO Implement dispersion effects, i.e. the change of a particle's
     transverse phase space coordinates on its relative momentum offset.
     For the moment, a NotImplementedError is raised if dispersion
-    coefficients are non-zero.
-    """
+    coefficients are non-zero. """
     def __init__(self,
             alpha_x_s0, beta_x_s0, D_x_s0, alpha_x_s1, beta_x_s1, D_x_s1,
             alpha_y_s0, beta_y_s0, D_y_s0, alpha_y_s1, beta_y_s1, D_y_s1,
             dQ_x, dQ_y, *segment_detuners):
-        """
-        The values of the TWISS parameters alpha_{x,y} and beta_{x,y} as
+        """ Return an instance of the TransverseSegmentMap class. The
+        values of the TWISS parameters alpha_{x,y} and beta_{x,y} as
         well as of the dispersion coefficients D_{x,y} (not yet
         implemented) are given at the beginning s0 and at the end s1 of
         the corresponding segment. The dQ_{x,y} denote the betatron
@@ -44,36 +41,25 @@ class TransverseSegmentMap(object):
         SegmentDetuner objects present in this segment are passed and
         zipped to a list via the argument *segment_detuners.
         The matrices self.I and self.J are constant and are calculated
-        only once at instantiation of the TransverseSegmentMap.
-        """
+        only once at instantiation of the TransverseSegmentMap. """
         self._build_segment_map(alpha_x_s0, beta_x_s0, alpha_x_s1, beta_x_s1,
                                 alpha_y_s0, beta_y_s0, alpha_y_s1, beta_y_s1)
         self.dQ_x = dQ_x
         self.dQ_y = dQ_y
         self.segment_detuners = segment_detuners
 
-        if any((D_x_s0, D_x_s1, D_y_s0, D_y_s1)) != 0:
+        if (D_x_s0 != 0 or D_x_s1 != 0 or D_y_s0 != 0 or D_y_s1 != 0):
             raise NotImplementedError('Non-zero values have been \n' +
                 'specified for the dispersion coefficients D_{x,y}.\n' +
-                'The effects of dispersion are not yet implemented. \n')
-
-        ''' Note that the following attributes self.alpha_{x,y} and
-        self.beta_{x,y} are required only by the matched bunch
-        initialisation. '''
-        self.alpha_x = alpha_x_s0
-        self.alpha_y = alpha_y_s0
-        self.beta_x = beta_x_s0
-        self.beta_y = beta_y_s0
+                'But, the effects of dispersion are not yet implemented. \n')
 
     def _build_segment_map(self, alpha_x_s0, beta_x_s0, alpha_x_s1, beta_x_s1,
                            alpha_y_s0, beta_y_s0, alpha_y_s1, beta_y_s1):
-        """
-        Calculate matrices I and J which are decoupled from the
-        phase advance and only depend on the TWISS parameters at the
-        boundaries of the accelerator segment. These matrices are
+        """ Calculate matrices I and J which are decoupled from the
+        phase advance dependency and only depend on the TWISS parameters
+        at the boundaries of the accelerator segment. These matrices are
         constant and hence need to be calculated only once at
-        instantiation of the TransverseSegmentMap.
-        """
+        instantiation of the TransverseSegmentMap. """
         self.I = np.zeros((4, 4))
         self.J = np.zeros((4, 4))
 
@@ -102,18 +88,16 @@ class TransverseSegmentMap(object):
         self.J[3,3] = -np.sqrt(beta_y_s0 / beta_y_s1) * alpha_y_s1
 
     def track(self, beam):
-        """
-        The dphi_{x,y} denote the phase advance in the horizontal and
-        vertical plane respectively for the given accelerator segment.
-        They are composed of the betatron tunes dQ_{x,y} and a possible
-        incoherent tune shift introduced by detuner elements / effects
-        defined in the list self.segment_detuners (they are all
-        instances of the SegmentDetuner child classes).
-        The transport matrix is defined by the coefficients M_{ij}.
-        """
+        """ The dphi_{x,y} denote the phase advance in the horizontal
+        and vertical plane respectively for the given accelerator
+        segment. They are composed of the betatron tunes dQ_{x,y} and a
+        possible incoherent tune shift introduced by detuner elements
+        / effects defined in the list self.segment_detuners (they are
+        all instances of the SegmentDetuner child classes).
+        The transport matrix is defined by the coefficients M_{ij}. """
 
-        ''' Calculate phase advance (betatron motion in this segment and
-        incoherent tune shifts introduced by detuning effects). '''
+        # Calculate phase advance (betatron motion in this segment and
+        # incoherent tune shifts introduced by detuning effects).
         dphi_x = self.dQ_x
         dphi_y = self.dQ_y
 
@@ -130,8 +114,8 @@ class TransverseSegmentMap(object):
         sin_dphi_x = sin(dphi_x)
         sin_dphi_y = sin(dphi_y)
 
-        ''' Calculate the matrix M and transport the transverse phase
-        spaces through the segment. '''
+        # Calculate the matrix M and transport the transverse phase
+        # spaces through the segment.
         M00 = self.I[0,0]*cos_dphi_x + self.J[0,0]*sin_dphi_x
         M01 = self.I[0,1]*cos_dphi_x + self.J[0,1]*sin_dphi_x
         M10 = self.I[1,0]*cos_dphi_x + self.J[1,0]*sin_dphi_x
@@ -146,45 +130,44 @@ class TransverseSegmentMap(object):
 
 
 class TransverseMap(object):
-    """
-    Collection class for TransverseSegmentMap objects. This class is
+    """ Collection class for TransverseSegmentMap objects. This class is
     used to define a one turn map for transverse particle tracking. An
     accelerator ring is divided into segments (1 or more). They are
     defined by the user with the array s containing the positions of
-    all the segment boundaries. At instantiation of the TransverseMap,
-    a TransverseSegmentMap object is created for each segment of the
-    accelerator and appended to the list self.segment_maps. When
-    generating the TransverseSegmentMaps, the influence of incoherent
-    detuning by effects defined in the trackers.detuners module is
-    included and the corresponding SegmentDetuner objects are generated
-    on the fly. Their strength of detuning is distributed proportionally
-    along the accelerator circumference.
-    Note that the TransverseMap only knows how to generate and store
-    TransverseSegmentMaps. It is not capable of tracking particles. The
-    transport mechanism of particles in the transverse plane is entirely
-    implemented in the TransverseSegmentMap class.
-    Note that the TransverseMap is implemented to act as a sequence,
-    hence the instances of the TransverseSegmentMap objects (stored in
+    all the segment boundaries. The TransverseMap stores all the
+    relevant parameters (optics) at each segment boundary. The first
+    boundary of the first segment is referred to as the injection
+    point.
+    At instantiation of the TransverseMap, a TransverseSegmentMap object
+    is created for each segment of the accelerator and appended to the
+    list self.segment_maps. When generating the TransverseSegmentMaps,
+    the influence of incoherent detuning by effects defined in the
+    trackers.detuners module is included and the corresponding
+    SegmentDetuner objects are generated on the fly. Their strength of
+    detuning is distributed proportionally along the accelerator
+    circumference.
+    Note that the TransverseMap only knows all the relevant optics
+    parameters needed to generate the TransverseSegmentMaps. It is not
+    capable of tracking particles. The transport mechanism of particles
+    in the transverse plane is entirely implemented in the
+    TransverseSegmentMap class.
+    Since the TransverseMap is implemented to act as a sequence, the
+    instances of the TransverseSegmentMap objects (stored in
     self.segment_maps) can be accessed using the notation
-    TransverseMap(...)[i] (with i the index of the accelerator segment). 
-    """
+    TransverseMap(...)[i] (with i the index of the accelerator
+    segment). """
     def __init__(self, s, alpha_x, beta_x, D_x, alpha_y, beta_y, D_y,
                  Q_x, Q_y, *detuner_collections):
-        """
-        Create a one-turn map that manages the tracking for each of the
-        accelerator segments defined by s.
-
+        """ Create a one-turn map that manages the transverse tracking
+        for each of the accelerator segments defined by s.
           s is the array of positions defining the boundaries of the
           segments for one turn.
-
           alpha_{x,y}, beta_{x,y} are the TWISS parameters alpha and
           beta. They are arrays of size len(s) as these parameters must
           be defined at every segment boundary of the accelerator.
-
           D_{x,y} are the dispersion coefficients. They are arrays of
           size len(s) as these parameters must be defined at every
           segment boundary of the accelerator.
-
           Q_{x,y} are scalar values and define the betatron tunes (i.e.
           the number of betatron oscillations in one complete turn).
 
@@ -193,9 +176,7 @@ class TransverseMap(object):
           knows how to generate and store its SegmentDetuner objects
           to 'distribute' the detuning proportionally along the
           accelerator circumference.
-
-        WARNING: Dispersion effects are not yet implemented.
-        """
+        WARNING: Dispersion effects are not yet implemented. """
         self.s = s
         self.alpha_x = alpha_x
         self.beta_x = beta_x
@@ -207,13 +188,12 @@ class TransverseMap(object):
         self.Q_y = Q_y
         self.detuner_collections = detuner_collections
 
-        ''' List to store TransverseSegmentMap objects. '''
+        # List to store TransverseSegmentMap objects.
         self.segment_maps = []
         self._generate_segment_maps()
 
     def _generate_segment_maps(self):
-        """
-        This method is called at instantiation of a TransverseMap
+        """ This method is called at instantiation of a TransverseMap
         object. For each segment of the accelerator ring (defined by the
         array self.s), a TransverseSegmentMap object is instantiated and
         appended to the list self.segment_maps. The creation of the
@@ -225,11 +205,10 @@ class TransverseMap(object):
         detuning strength of a SegmentDetuner, the one-turn detuning
         strength is scaled to the segment_length. Note that this
         quantity is given in relative units (i.e. it is normalized to
-        the accelerator circumference s[-1]).
-        """
+        the accelerator circumference s[-1]). """
         segment_length = np.diff(self.s) / self.s[-1]
         
-        ''' Betatron motion normalized to this particular segment. '''
+        # Betatron motion normalized to this particular segment.
         dQ_x = self.Q_x * segment_length
         dQ_y = self.Q_y * segment_length
 
@@ -238,12 +217,12 @@ class TransverseMap(object):
             s0 = seg % n_segments
             s1 = (seg + 1) % n_segments
 
-            ''' Instantiate SegmentDetuner objects. '''
+            # Instantiate SegmentDetuner objects.
             for detuner in self.detuner_collections:
                 detuner.generate_segment_detuner(segment_length[s0],
                     beta_x=self.beta_x[s0], beta_y=self.beta_y[s0])
             
-            ''' Instantiate TransverseSegmentMap objects. '''
+            # Instantiate TransverseSegmentMap objects.
             transverse_segment_map = TransverseSegmentMap(
                 self.alpha_x[s0], self.beta_x[s0], self.D_x[s0],
                 self.alpha_x[s1], self.beta_x[s1], self.D_x[s1],
@@ -255,10 +234,9 @@ class TransverseMap(object):
             self.segment_maps.append(transverse_segment_map)
 
     def get_injection_optics(self):
-        '''Return a tuple with the transverse TWISS parameters
+        """ Return a tuple with the transverse TWISS parameters
         (alpha_x, beta_x, alpha_y, beta_y) from the beginning of the
-        first segment.
-        '''
+        first segment (injection point). """
         return (self.alpha_x[0], self.beta_x[0],
                 self.alpha_y[0], self.beta_y[0])
 
