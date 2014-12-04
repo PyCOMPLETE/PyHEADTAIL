f8987658efcf80030eae972c72c8662f6472f145
michuschenk <michuschenk@gmail.com>
2014-12-04 19:52:05 +0100

    Add particles.cython_functions cython module. Adapt cython setup.py accordingly.
     - particles.cython_functions includes a function to loop through
       the particles in the bunch and relocate lost particles to the
       end of the coords and momenta arrays.

--- setup.py
+++ setup.py
@@ -114,6 +114,14 @@ cy_ext = [
                  extra_link_args=["-fopenmp"],
                  #extra_compile_args=["-g"],
                  #extra_link_args=["-g"],
+                 ),
+        Extension("particles.cython_functions",
+                 ["particles/cython_functions.pyx"],
+                 include_dirs=[np.get_include()], library_dirs=[], libraries=["m"],
+                 #extra_compile_args=["-fopenmp"],
+                 #extra_link_args=["-fopenmp"],
+                 #extra_compile_args=["-g"],
+                 #extra_link_args=["-g"],
                  )
           ]
 
