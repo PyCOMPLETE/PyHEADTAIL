277bddd971ff42371693c9d5b7f9683ab93bf605
Adrian Oeftiger <oeftiger@cern.ch>
2015-08-04 15:52:54 +0200

    in gpu/__init__.py leave normal Slicers alone, if automatic conversion for Slicers to GPU is sought, need to write a UniformBinSlicer adaption / wrapper to create a mesh for the MeshSlicer! Not yet done.

--- gpu/__init__.py
+++ gpu/__init__.py
@@ -3,18 +3,23 @@
 
 from skcuda.misc import mean, std, diff
 from pycuda import gpuarray
+from pycuda import cumath
 
+# I. subjects to monkey patching:
 from ..particles import particles as def_particles
 from ..particles import slicing as def_slicing
+from ..trackers import simple_long_tracking as def_simple_long_tracking
 
-# Particles rebindings for GPU
+# II. actual monkey patching
+
+# a) Particles rebindings for GPU
 from .particles import ParticlesGPU
 
 def_particles.Particles = ParticlesGPU
 def_particles.mean = lambda *args, **kwargs: mean(*args, **kwargs).get()
 def_particles.std = lambda *args, **kwargs: std(*args, **kwargs).get()
 
-# Slicing rebindings for GPU
+# b) Slicing rebindings for GPU
 # def_slicing.min_ = lambda *args, **kwargs: gpuarray.min(*args, **kwargs).get()
 # def_slicing.max_ = lambda *args, **kwargs: gpuarray.max(*args, **kwargs).get()
 # def_slicing.diff = diff
@@ -25,3 +30,6 @@ from .slicing import SlicerGPU
 # # (to solve the corresponding need to replace all inheriting classes' parent!)
 # def_slicing.Slicer = SlicerGPU
 # def_slicing.UniformBinSlicer.__bases__ = (SlicerGPU,)
+
+# c) Longitudinal tracker rebindings for GPU
+def_simple_long_tracking.sin = cumath.sin
