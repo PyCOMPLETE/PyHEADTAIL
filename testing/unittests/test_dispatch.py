2200161ba842ccbafbc9e5758b6fc03a45a53647
Stefannn <stefanhegglin@gmail.com>
2015-10-16 12:18:36 +0200

    Test for equiv of GPU/CPU pmath
    - Check all common CPU/GPU functions available in the pmath
      module for consistency between CPU/GPU

--- testing/unittests/test_dispatch.py
+++ testing/unittests/test_dispatch.py
@@ -17,6 +17,15 @@ import numpy as np
 
 import PyHEADTAIL.general.pmath as pm
 
+# try to import pycuda, if not available --> skip this test file
+try:
+    import pycuda.autoinit
+    import pycuda.gpuarray
+except ImportError:
+    has_pycuda = False
+else:
+    has_pycuda = True
+
 class TestDispatch(unittest.TestCase):
     '''Test Class for the function dispatch functionality in general.pmath'''
     def setUp(self):
@@ -43,6 +52,26 @@ class TestDispatch(unittest.TestCase):
             'Setting the active dict to GPU fails. Not all CPU functions ' +
             'were deleted from pm.globals() when switching to GPU.'
             )
+
+    @unittest.skipUnless(has_pycuda, 'pycuda not found')
+    def test_equivalency_CPU_GPU_functions(self):
+        '''
+        Check that CPU/GPU functions yield the same result (if both exist)
+        No complete tracking, only bare functions.
+        '''
+        np.random.seed(0)
+        parameter_cpu = np.random.normal(loc=1., scale=1., size=1000)
+        parameter_gpu = pycuda.gpuarray.to_gpu(parameter_cpu)
+        print type(parameter_gpu)
+        common_functions = [fn for fn in self.available_CPU
+                            if fn in self.available_GPU]
+        for fname in common_functions:
+            res_cpu = pm._CPU_numpy_func_dict[fname](parameter_cpu)
+            res_gpu = pm._GPU_func_dict[fname](parameter_gpu)
+            if isinstance(res_gpu, pycuda.gpuarray.GPUArray):
+                res_gpu = res_gpu.get()
+            self.assertTrue(np.allclose(res_cpu, res_gpu),
+                'CPU/GPU version of ' + fname + ' dont yield the same result')
 
     def tearDown(self):
         pass
