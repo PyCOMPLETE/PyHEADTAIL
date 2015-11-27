1c83130851f146d5acd740a4c0510910eddc8d46
Stefannn <stefanhegglin@gmail.com>
2015-11-27 15:47:47 +0100

    gpu_utils: init skcuda.misc
    - only required if you want to use cublas etc,
      but nice to have for no cost

--- gpu/gpu_utils.py
+++ gpu/gpu_utils.py
@@ -17,10 +17,15 @@ try:
         has_pycuda=False
 except ImportError:
     has_pycuda = False
-
+import atexit
 ################################################################################
 
 if has_pycuda:
     memory_pool = pycuda.tools.DeviceMemoryPool()
+
+    import skcuda.misc #s
+    skcuda.misc.init(allocator=memory_pool.allocate)
+    atexit.register(skcuda.misc.shutdown)
+
     n_streams = 3
     streams = [drv.Stream() for i in xrange(n_streams)]
