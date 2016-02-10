654c0ecbceb0292ad79e9b904e26666584fb5f2d
Adrian Oeftiger <oeftiger@cern.ch>
2016-02-10 11:46:09 +0100

    thrust_code: removed helper and min-max function.

--- gpu/thrust_code.cu
+++ gpu/thrust_code.cu
@@ -5,7 +5,7 @@
 #include <thrust/copy.h>
 #include <thrust/device_ptr.h>
 #include <thrust/sequence.h>
-namespace helper {struct between{const int minimum;const int maximum;__host__ __device__ between(int min, int max): minimum(min), maximum(max) {};__host__ __device__ bool operator()(const int x) {return !(x < minimum) && !(maximum < x);};};};
+// namespace helper {struct between{const int minimum;const int maximum;__host__ __device__ between(int min, int max): minimum(min), maximum(max) {};__host__ __device__ bool operator()(const int x) {return !(x < minimum) && !(maximum < x);};};};
 
 extern "C" { //required to avoid name mangling in PyCUDA: https://devtalk.nvidia.com/default/topic/471412/pycuda-thrust-example-in-case-someone-is-curious/
 
@@ -62,18 +62,18 @@ void thrust_upper_bound_int(int* sorted_ptr, int sorted_length, int* bounds_ptr,
   thrust::device_ptr<int> thrust_output_ptr(output_ptr);
   thrust::upper_bound(thrust_sorted_ptr, thrust_sorted_ptr + sorted_length, thrust_bounds_ptr, thrust_bounds_ptr + bounds_length, thrust_output_ptr);
 }
-int thrust_copy_if_min_max(int* input_ptr, int input_length, int* output_ptr, int mini, int maxi)
-{
-  thrust::device_ptr<int> d_input_ptr(input_ptr);
-  thrust::device_ptr<int> d_output_ptr(output_ptr);
-  thrust::device_ptr<int> d_end_input_ptr(d_input_ptr+input_length);
-  thrust::device_ptr<int> output_end = thrust::copy_if(d_input_ptr, d_end_input_ptr, d_output_ptr, helper::between(mini, maxi));
-  int output_length = output_end - d_output_ptr;
-  return output_length;
-}
+// int thrust_copy_if_min_max(int* input_ptr, int input_length, int* output_ptr, int mini, int maxi)
+// {
+//   thrust::device_ptr<int> d_input_ptr(input_ptr);
+//   thrust::device_ptr<int> d_output_ptr(output_ptr);
+//   thrust::device_ptr<int> d_end_input_ptr(d_input_ptr+input_length);
+//   thrust::device_ptr<int> output_end = thrust::copy_if(d_input_ptr, d_end_input_ptr, d_output_ptr, helper::between(mini, maxi));
+//   int output_length = output_end - d_output_ptr;
+//   return output_length;
+// }
 
-// TEST, REMOVE IF WORKING
-__host__ int my_test(int n) {
-    return n+1;
-}
+// // TEST, REMOVE IF WORKING
+// __host__ int my_test(int n) {
+//     return n+1;
+// }
 } // end extern "C"
