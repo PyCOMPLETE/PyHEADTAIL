a6c532651ab108a5a740760d8a437112ec81e690
Adrian Oeftiger <oeftiger@cern.ch>
2016-06-13 14:44:33 +0200

    Transverse Efield Map: changed argument interface.
    It accepts a Slicer instance instead of fixed slicer
    instantiation arguments now, allows for more flexibility.

--- field_maps/Transverse_Efield_map.py
+++ field_maps/Transverse_Efield_map.py
@@ -4,76 +4,63 @@ from scipy.constants import c
 from PyHEADTAIL.particles.slicing import UniformBinSlicer
 from PyPIC.PyPIC_Scatter_Gather import PyPIC_Scatter_Gather
 
+from . import Element
 
+class Transverse_Efield_map(Element):
+    def __init__(self, xg, yg, Ex, Ey, L_interaction, slicer,
+        	flag_clean_slices=False, wrt_slice_centroid=False,
+        	x_beam_offset=0., y_beam_offset=0., *args, **kwargs):
 
-class Transverse_Efield_map(object):
-	def __init__(self, xg, yg, Ex, Ey, n_slices, z_cut,
-		L_interaction, flag_clean_slices = False, wrt_slice_centroid = False,
-		x_beam_offset = 0., y_beam_offset = 0.):
-			
-		if type(z_cut) is float:
-			z_cuts = (-z_cut, z_cut)
-		elif type(z_cut) is tuple:
-			z_cuts = z_cut
-		else:
-			raise ValueError('Type not recognized!')
+        self.slicer = slicer
+        self.L_interaction = L_interaction
+        self.flag_clean_slices = flag_clean_slices
+        self.wrt_slice_centroid = wrt_slice_centroid
 
-		self.slicer = UniformBinSlicer(n_slices = n_slices, z_cuts=z_cuts)
-		self.L_interaction = L_interaction
-		self.flag_clean_slices = flag_clean_slices
-		self.wrt_slice_centroid = wrt_slice_centroid
+        self.Ex = Ex
+        self.Ey = Ey
+        self.pic = PyPIC_Scatter_Gather(xg=xg, yg=yg)
 
-		self.Ex = Ex
-		self.Ey = Ey
-		self.pic = PyPIC_Scatter_Gather(xg=xg, yg=yg)
-		
-		self.x_beam_offset = x_beam_offset
-		self.y_beam_offset = y_beam_offset
+        self.x_beam_offset = x_beam_offset
+        self.y_beam_offset = y_beam_offset
 
+    def get_beam_x(self, beam):
+        return beam.x
 
-	def get_beam_x(self, beam):
-		return beam.x
+    def get_beam_y(self, beam):
+        return beam.y
 
-	def get_beam_y(self, beam):
-		return beam.y
+    def track(self, beam):
+        if self.flag_clean_slices:
+            beam.clean_slices()
 
-#	@profile
-	def track(self, beam):
+        slices = beam.get_slices(self.slicer)
 
+        for sid in xrange(slices.n_slices-1, -1, -1):
 
-		if self.flag_clean_slices:
-			beam.clean_slices()
+            # select particles in the slice
+            pid = slices.particle_indices_of_slice(sid)
 
-		slices = beam.get_slices(self.slicer)
+            # slice size
+            dz = (slices.z_bins[sid + 1] - slices.z_bins[sid])
 
+            x = self.get_beam_x(beam)[pid]
+            y = self.get_beam_y(beam)[pid]
 
-		for i in xrange(slices.n_slices-1, -1, -1):
+            self.pic.efx = np.squeeze(self.Ex[sid,:,:])
+            self.pic.efy = np.squeeze(self.Ey[sid,:,:])
 
-			# select particles in the slice
-			ix = slices.particle_indices_of_slice(i)
+            centroid_x = 0
+            centroid_y = 0
+            if self.wrt_slice_centroid:
+                centroid_x = np.mean(x)
+                centroid_y = np.mean(y)
 
-			# slice size and time step
-			dz = (slices.z_bins[i + 1] - slices.z_bins[i])
-			#dt = dz / (beam.beta * c)
+            Ex_sc_p, Ey_sc_p = self.pic.gather(
+                x - centroid_x + self.x_beam_offset,
+                y - centroid_y + self.y_beam_offset
+            )
 
-			x = self.get_beam_x(beam)[ix]
-			y = self.get_beam_y(beam)[ix]
-
-			self.pic.efx = np.squeeze(self.Ex[i,:,:])
-			self.pic.efy = np.squeeze(self.Ey[i,:,:])
-			if self.wrt_slice_centroid:
-				Ex_sc_p, Ey_sc_p = self.pic.gather(x-np.mean(x)+self.x_beam_offset, y-np.mean(y)+self.y_beam_offset)
-			else:
-				Ex_sc_p, Ey_sc_p = self.pic.gather(x+self.x_beam_offset, y+self.y_beam_offset)
-
-			## kick beam particles
-			fact_kick = beam.charge/(beam.mass*beam.beta*beam.beta*beam.gamma*c*c)*self.L_interaction
-			beam.xp[ix]+=fact_kick*Ex_sc_p
-			beam.yp[ix]+=fact_kick*Ey_sc_p
-
-
-
-
-
-
-
+            # kick beam particles
+            fact_kick = beam.charge / (beam.p0*beam.beta*c) * self.L_interaction
+            beam.xp[pid] += fact_kick * Ex_sc_p
+            beam.yp[pid] += fact_kick * Ey_sc_p
