c0cc8c9699e39ca38290ba7fb55e2fd5baed137b
Adrian Oeftiger <oeftiger@cern.ch>
2016-06-21 11:43:46 +0200

    PyPIC_spacecharge: united 2.5D and 3D in one class.
    Kicks are consistent between 2.5D, 3D and Gianni's
    transverse_spacecharge.py for the FFTSolver on PyPIC 2.0
    (with dx and dy separated). Also the stored e-fields
    have the same units, they are given in beam frame.
    For SPS at injection, a KV beam gives 15kV/m maximum
    E_y field for both 2.5D/3D PIC which matches the analytic formula.

--- spacecharge/pypic_spacecharge.py
+++ spacecharge/pypic_spacecharge.py
@@ -3,6 +3,7 @@ space charge modelling in transverse 2.5D and 3D.
 
 PyPIC can be found under
 https://github.com/PyCOMPLETE/PyPIC .
+NB: the (feature/redesign) branch is required for this!
 
 @authors: Adrian Oeftiger
 @date: 18.01.2016
@@ -15,23 +16,38 @@ from scipy.constants import c
 
 from . import Element
 
-from PyPIC.meshing import RectMesh3D
-
 from ..general import pmath as pm
 
 
-class SpaceCharge25D(Element):
-    '''Transverse slice-by-slice (2.5D) space charge using a
-    particle-in-cell algorithm via PyPIC. Uses a fixed 3D mesh
-    along slices.
+def align_particles(beam, mesh_3d):
+    '''Sort all particles by their mesh node IDs.'''
+    ids = mesh_3d.get_node_ids(beam.x, beam.y, beam.z_beamframe)
+    permutation = pm.argsort(ids)
+    beam.reorder(permutation)
+    # node id array has changed by now!
+
+def get_bounds(beam, mesh_3d):
+    '''Determine indices of sorted particles for each cell, i.e.
+    lower and upper index bounds.
+    '''
+    seq = pm.seq(mesh_3d.n_nodes)
+    ids = mesh_3d.get_node_ids(beam.x, beam.y, beam.z_beamframe)
+    lower_bounds = pm.searchsortedleft(ids, seq)
+    upper_bounds = pm.searchsortedright(ids, seq)
+    return lower_bounds, upper_bounds
+
+class SpaceChargePIC(Element):
+    '''Transverse slice-by-slice (2.5D) or full (3D) space charge using
+    a particle-in-cell algorithm via PyPIC. Uses a fixed 3D mesh
+    with respect to beam.z_beamframe , i.e. the mesh does not adapt
+    and remains constant.
     '''
 
-    def __init__(self, slicer, length, pypic_algorithm, sort_particles=False,
+    def __init__(self, length, pypic_algorithm, sort_particles=False,
                  *args, **kwargs):
         '''Arguments:
-            - slicer: particles.Slicer instance.
-            - length: interaction length over which the space charge is
-              integrated.
+            - length: interaction length over which the space charge
+              force is integrated.
             - pypic_algorithm: PyPIC.pypic.PyPIC(_GPU) instance which
               has the particle-in-cell algorithm encoded. This has to
               be set up by the user beforehand (i.e. the mesh,
@@ -45,166 +61,49 @@ class SpaceCharge25D(Element):
               (NB: sort_particles=True is necessarily required for the
                PyPIC_GPU.sorted_particles_to_mesh method.)
         '''
-        self.slicer = slicer
         self.length = length
         self.pypic = pypic_algorithm
         self.sort_particles = sort_particles
+        self.is_25D = getattr(self.pypic.poissonsolver, 'is_25D', False)
         if self.pypic.mesh.dimension != 3:
             raise RuntimeError(
-                '2.5D space charge requires a three-dimensional mesh!')
+                '2.5D / 3D space charge requires a three-dimensional mesh!')
 
-    # @staticmethod
-    # def _create_3d_mesh(mesh_2d, z_cut_tail, z_cut_head, n_slices):
-    #     '''For sorting purposes, in order for each slice to have all
-    #     particles sorted by their transverse 2D mesh node ID.
-    #     '''
-    #     dz = (z_cut_head - z_cut_tail) / float(n_slices)
-    #     origin = (mesh_2d.x0, mesh_2d.y0, z_cut_tail)
-    #     distances = (mesh_2d.dx, mesh_2d.dy, dz)
-    #     n_cells_per_direction = (mesh_2d.nx, mesh_2d.ny, n_slices)
-    #     return RectMesh3D(origin, distances, n_cells_per_direction,
-    #                       mathlib=mesh_2d.mathlib)
-
-    @staticmethod
-    def align_particles(beam, mesh_3d):
-        '''Sort all particles by their transverse sub-mesh node IDs via
-        the given 3D mesh.
-        '''
-        ids = mesh_3d.get_node_ids(beam.x, beam.y, beam.z)
-        permutation = pm.argsort(ids)
-        beam.reorder(permutation)
-        # node ids have changed by now!
-
-    @staticmethod
-    def get_bounds(beam, mesh_3d, idx_relevant_particles):
-        '''Determine indices of sorted particles for each cell, i.e.
-        lower and upper index bounds.
-        '''
-        seq = pm.arange(pm.zeros(1, dtype=np.int32),
-                        mesh_3d.n_nodes,
-                        pm.ones(1, dtype=np.int32),
-                        mesh.n_nodes, dtype=np.int32)
-        ids = mesh_3d.get_node_ids(
-            pm.take(beam.x, idx_relevant_particles),
-            pm.take(beam.y, idx_relevant_particles),
-            pm.take(beam.z, idx_relevant_particles))
-        lower_bounds = pm.searchsortedleft(ids, seq)
-        upper_bounds = pm.searchsortedright(ids, seq)
-        return lower_bounds, upper_bounds
-
-    def track(self, beam):
-        slices = self.slicer.slice(beam)
-
-        if self.sort_particles:
-            # mesh_3d = self._create_3d_mesh(self.pypic.mesh, slices.z_cut_tail,
-            #                                slices.z_cut_head, slices.n_slices)
-            self.align_particles(beam, self.pypic.mesh)
-
-        # scale to macro-particle charges, integrate over length
-        kick_factor = (self.length / (beam.beta*c) *
-                       beam.charge_per_mp**2 / beam.p0)
-
-        # last slice is always empty!
-        for (sid, n_mp_in_slice) in enumerate(
-                slices.n_macroparticles_per_slice[:-1]):
-
-            pids_of_slice = slices.particle_indices_of_slice(sid)
-            solve_kwargs = {
-                'charge': beam.charge_per_mp,
-            }
-            if self.sort_particles:
-                solve_kwargs['lower_bounds'], solve_kwargs['upper_bounds'] = \
-                    self.get_bounds(beam, self.pypic.mesh, pids_of_slice)
-
-            en_x, en_y = self.pypic.pic_solve(beam.x, beam.y, beam.z,
-                                              **solve_kwargs)
-
-            en_x *= beam.gamma**-2
-            en_y *= beam.gamma**-2
-
-            beam.xp += en_x * kick_factor
-            beam.yp += en_y * kick_factor
-
-
-
-class SpaceCharge3D(Element):
-    '''Space charge in all three planes using a particle-in-cell
-    algorithm via PyPIC.
-    The 3D mesh does not adapt and remains constant.
-    '''
-
-    def __init__(self, length, pypic_algorithm, sort_particles=False,
-                 *args, **kwargs):
-        '''Arguments:
-            - length: interaction length over which the space charge is
-              integrated.
-            - mesh_nx: horizontal mesh size (for the mesh to be created)
-            - mesh_ny: vertical mesh size (for the mesh to be created)
-
-        Optional arguments:
-            - pypic_algorithm: pre-configured PyPIC.pypic.PyPIC(_GPU)
-              instance with the particle-in-cell algorithm encoded, has
-              to be consistently set it up beforehand (i.e. the mesh
-              w.r.t. the poisson solver, particle-to-mesh
-              deposition method w.r.t. sort_particles etc.).
-            - sort_particles: determines whether to sort the particles
-              by their mesh ID. This may speed up the PyPIC
-              particle-to-mesh and mesh-to-particles methods
-              due to coalesced memory access, especially on the GPU.
-
-              (NB: sort_particles=True is necessarily required for the
-               PyPIC_GPU.sorted_particles_to_mesh method.)
-        '''
-        self.length = length
-        self.sort_particles = sort_particles
-        if pypic_algorithm.mesh.dimension != 3:
-            raise RuntimeError('3D space charge requires a three-dimensional '
-                               'mesh!')
-        self.pypic = pypic_algorithm
-
-    @staticmethod
-    def align_particles(beam, mesh):
-        '''Sort all particles by their mesh node IDs.'''
-        ids = mesh.get_node_ids(beam.x, beam.y, beam.z_beamframe)
-        permutation = pm.argsort(ids)
-        beam.reorder(permutation)
-        # node id array has changed by now!
-
-    @staticmethod
-    def get_bounds(beam, mesh):
-        '''Determine indices of sorted particles for each cell, i.e.
-        lower and upper index bounds.
-        '''
-        seq = pm.seq(mesh.n_nodes)
-        ids = mesh.get_node_ids(beam.x, beam.y, beam.z_beamframe)
-        lower_bounds = pm.searchsortedleft(ids, seq)
-        upper_bounds = pm.searchsortedright(ids, seq)
-        return lower_bounds, upper_bounds
-
-    def track(self, beam):
+    def track(self, beam, pypic_state=None):
         mesh = self.pypic.mesh
 
         solve_kwargs = {
             'charge': beam.charge_per_mp,
+            'state': pypic_state,
         }
+        if self.is_25D:
+            # 2.5D: macro-particle charge becomes line density in beam frame
+            # (in 3D this is implicit via rho=mesh_charges/mesh_3d.volume_elem)
+            solve_kwargs['charge'] /= mesh.dz
+
         if self.sort_particles:
-            self.align_particles(beam, mesh)
+            align_particles(beam, mesh)
 
             solve_kwargs['lower_bounds'], solve_kwargs['upper_bounds'] = \
-                self.get_bounds(beam, mesh)
+                get_bounds(beam, mesh)
 
-        # charge normalised electric fields in beam frame [V/m / Coul]
-        en_x, en_y, en_z = self.pypic.pic_solve(
+        # electric fields for each particle in beam frame [V/m]
+        force_fields = self.pypic.pic_solve(
             beam.x, beam.y, beam.z_beamframe, **solve_kwargs)
 
-        # Lorentz boost to lab frame --> magnetic fields:
-        en_x *= beam.gamma**-2
-        en_y *= beam.gamma**-2
+        # we want force F = q * (1 - beta^2) E_r where E_r is in lab frame
+        # --> Lorentz boost E_r from beam frame to lab frame (*gamma)
+        # --> include magnetic fields (1 - beta^2) = 1/gamma^2
+        # ==> overall factor 1/gamma
+        force_fields[0] /= beam.gamma
+        force_fields[1] /= beam.gamma
 
-        # scale to macro-particle charges, integrate over length
-        kick_factor = (self.length / (beam.beta*c) *
-                       beam.charge_per_mp / beam.p0)
+        # integrate over dt and apply the force to each charged particle,
+        # p0 comes from kicking xp=p_x/p0 instead of p_x
+        kick_factor = (self.length / (beam.beta*c) * beam.charge / beam.p0)
 
-        beam.xp += en_x * kick_factor
-        beam.yp += en_y * kick_factor
-        beam.dp += en_z * kick_factor
+        beam.xp += force_fields[0] * kick_factor
+        beam.yp += force_fields[1] * kick_factor
+        if not self.is_25D:
+            beam.dp += force_fields[2] * kick_factor
+
