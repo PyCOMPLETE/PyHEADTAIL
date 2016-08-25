9bab13937c06b108c3d3079ddbce070d40b14304
like2000 <kevin.li.cern@gmail.com>
2016-08-25 11:16:33 +0200

    Update and set 4 cores for trig functions evaluation by default.

--- setup.py
+++ setup.py
@@ -94,6 +94,11 @@ cy_ext = [
               ["cobra_functions/c_sin_cos.pyx"],
               include_dirs=[np.get_include()],
               library_dirs=[], libraries=["m"],
+              extra_compile_args=["-fopenmp"], extra_link_args=["-fopenmp"]),
+    Extension("cobra_functions.interp_sin_cos",
+              ["cobra_functions/interp_sin_cos.pyx"],
+              include_dirs=[np.get_include()],
+              library_dirs=[], libraries=["m"],
               extra_compile_args=["-fopenmp"], extra_link_args=["-fopenmp"])
 ]
 
