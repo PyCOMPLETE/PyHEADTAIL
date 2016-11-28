ae5bfedc7fd85c9982a7e13702f22cd3094a664f
Adrian Oeftiger <oeftiger@cern.ch>
2016-11-28 17:09:25 +0100

    merging develop: general/* stuff

--- general/decorators.py
+++ general/decorators.py
@@ -5,15 +5,36 @@
 Provide useful decorators for PyHEADTAIL.
 '''
 
+import warnings
 from functools import wraps
 from ..gpu import gpu_utils
 
+
+def deprecated(message):
+    '''Deprecation warning as described in warnings documentaion.
+    '''
+    def deprecated_decorator(func):
+        @wraps(func)
+        def deprecated_wrapper(*args, **kwargs):
+            warnings.simplefilter('always', DeprecationWarning)
+            warnings.warn('\n\n*** DEPRECATED function: ' +
+                          '"{:s}" '.format(func.__name__) +
+                          'will be replaced in one of the future ' +
+                          'PyHEADTAIL releases!',
+                          category=DeprecationWarning, stacklevel=2)
+            warnings.simplefilter('default', DeprecationWarning)
+            print message
+            return func(*args, **kwargs)
+        return deprecated_wrapper
+    return deprecated_decorator
+
+
 def memoize(function):
-    '''Memoizes the output of a function for given arguments
-    (no keyword arguments) and returns the correspondingly saved value
-    after the first evaluation.
+    '''Memoizes the output of a function for given arguments (no keyword arguments)
+    and returns the correspondingly saved value after the first evaluation.
     '''
     store = {}
+
     @wraps(function)
     def evaluate(*args):
         signature = (args)
