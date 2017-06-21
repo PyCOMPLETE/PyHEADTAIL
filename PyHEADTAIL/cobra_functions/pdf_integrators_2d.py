6c5ff83eb9155386d31f4699a910c7647f2b6e3a
Adrian Oeftiger <oeftiger@cern.ch>
2017-06-21 15:19:51 +0200

    pdf_integrators_2d: adding cumtrapz functionality
    In analogy to quad functions, the cumtrapz functions
    provide moment integration up to covariance 2nd order.
    
    scipy.integrate.romberg is left in a functioning state
    for the standard deviation but not adapted to the
    structure of the quad and cumtrapz approaches.
    
    cumtrapz has been tested with all available longitudinal
    distribution types in the RFBucketMatcher, i.e.
    - thermal
    - qGaussian
    - parabolic
    - waterbag (!!! this works only with cumtrapz, not with quad!)
    
    cumtrapz makes any distribution type converge quickly but
    provides slightly less accurate matching (difference to
    quad is below 1e-4 in resulting sigma_z or epsn_z), which
    is negligible given the usual numerical statistics noise
    in macro-particle generation.

--- PyHEADTAIL/cobra_functions/pdf_integrators_2d.py
+++ PyHEADTAIL/cobra_functions/pdf_integrators_2d.py
@@ -122,71 +122,174 @@ def compute_cov_quad(psi, ylimit_min, ylimit_max, xmin, xmax):
 
     return V_x, C_xy, V_y
 
-def compute_zero_cumtrapz(psi, ylimits, xmin, xmax, n_samples=257):
+### scipy.integrate.cumtrapz moment integration:
 
-    x_arr = np.linspace(xmin, xmax, n_samples)
+def compute_zero_cumtrapz(psi, ylimit_min, ylimit_max, xmin, xmax,
+                          n_samples=513):
+    '''Compute the zeroth moment of the distribution function psi from
+    xmin to xmax between the contours ylimit_min and ylimit_max
+    (e.g. an RFBucket separatrix) using the numerical
+    scipy.integrate.cumtrapz integration method.
+
+    Arguments:
+        - psi: 2D distribution function with two arguments x and y
+        - ylimit_min, ylimit_max: contour functions yielding the lower
+          and the upper y(x) limit for given x
+        - xmin, xmax: lower and upper limit in the first argument of psi
+        - n_samples: integer number of sampling points for integration
+    '''
+
+    x_arr = np.linspace(xmin, xmax, num=n_samples)
     dx = x_arr[1] - x_arr[0]
 
     Q = 0
     for x in x_arr:
-        y = np.linspace(0, ylimits(x), n_samples)
+        y = np.linspace(ylimit_min(x), ylimit_max(x), num=n_samples)
         z = psi(x, y)
         Q += cumtrapz(z, y)[-1]
     Q *= dx
 
     return Q
 
-def compute_mean_cumtrapz(psi, ylimits, xmin, xmax, n_samples=257):
+def compute_mean_cumtrapz(psi, ylimit_min, ylimit_max, xmin, xmax,
+                          direction='x', n_samples=513):
+    '''Compute the first moment of the distribution function psi from
+    xmin to xmax between the contours ylimit_min and ylimit_max
+    (e.g. an RFBucket separatrix) using the numerical
+    scipy.integrate.cumtrapz integration method.
 
-    Q = compute_zero_cumtrapz(psi, ylimits, xmin, xmax, n_samples)
+    Arguments:
+        - psi: 2D distribution function with two arguments x and y
+        - ylimit_min, ylimit_max: contour functions yielding the lower
+          and the upper y(x) limit for given x
+        - xmin, xmax: lower and upper limit in the first argument of psi
+        - direction: 'x' or 'y' for calculating the mean in the first
+          or second argument of psi, respectively (default: 'x')
+        - n_samples: integer number of sampling points for integration
+    '''
 
-    x_arr = np.linspace(xmin, xmax, n_samples)
+    Q = compute_zero_cumtrapz(psi, ylimit_min, ylimit_max, xmin, xmax,
+                              n_samples)
+
+    x_arr = np.linspace(xmin, xmax, num=n_samples)
     dx = x_arr[1] - x_arr[0]
+
+    if direction is 'x':
+        f = lambda x, y: x * psi(x, y)
+    elif direction is 'y':
+        f = lambda x, y: y * psi(x, y)
+    else:
+        raise ValueError('direction needs to be either "x" or "y".')
 
     M = 0
     for x in x_arr:
-        y = np.linspace(0, ylimits(x), n_samples)
-        z = x * psi(x, y)
+        y = np.linspace(ylimit_min(x), ylimit_max(x), num=n_samples)
+        z = f(x, y)
         M += cumtrapz(z, y)[-1]
     M *= dx
 
     return M/Q
 
-def compute_std_cumtrapz(psi, ylimits, xmin, xmax, n_samples=257):
-    '''
-    Compute the variance of the distribution function psi from xmin
-    to xmax along the contours ylimits using numerical integration
-    methods.
+def compute_var_cumtrapz(psi, ylimit_min, ylimit_max, xmin, xmax,
+                         direction='x', n_samples=513):
+    '''Compute the second moment (variance) with respect to the x or
+    y direction of the distribution function psi from xmin to xmax
+    between the contours ylimit_min and ylimit_max
+    (e.g. an RFBucket separatrix) using the numerical
+    scipy.integrate.cumtrapz integration method.
+
+    Arguments:
+        - psi: 2D distribution function with two arguments x and y
+        - ylimit_min, ylimit_max: contour functions yielding the lower
+          and the upper y(x) limit for given x
+        - xmin, xmax: lower and upper limit in the first argument of psi
+        - direction: 'x' or 'y' for calculating the mean in the first
+          or second argument of psi, respectively (default: 'x')
+        - n_samples: integer number of sampling points for integration
     '''
 
-    Q = compute_zero_cumtrapz(psi, ylimits, xmin, xmax, n_samples)
-    M = compute_mean_cumtrapz(psi, ylimits, xmin, xmax, n_samples)
+    Q = compute_zero_cumtrapz(psi, ylimit_min, ylimit_max, xmin, xmax,
+                              n_samples)
+    M = compute_mean_cumtrapz(psi, ylimit_min, ylimit_max, xmin, xmax,
+                              direction, n_samples)
 
-    x_arr = np.linspace(xmin, xmax, n_samples)
+    x_arr = np.linspace(xmin, xmax, num=n_samples)
     dx = x_arr[1] - x_arr[0]
+
+    if direction is 'x':
+        f = lambda x, y: (x - M)**2 * psi(x, y)
+    elif direction is 'y':
+        f = lambda x, y: (y - M)**2 * psi(x, y)
+    else:
+        raise ValueError('direction needs to be either "x" or "y".')
 
     V = 0
     for x in x_arr:
-        y = np.linspace(0, ylimits(x), n_samples)
-        z = (x-M)**2 * psi(x, y)
+        y = np.linspace(ylimit_min(x), ylimit_max(x), num=n_samples)
+        z = f(x, y)
         V += cumtrapz(z, y)[-1]
     V *= dx
 
-    return np.sqrt(V/Q)
+    return V/Q
 
-def compute_std_romberg(psi, ylimits, xmin, xmax, n_samples=257):
-    '''
-    Compute the variance of the distribution function psi from xmin
-    to xmax along the contours ylimits using numerical integration
-    methods.
+def compute_cov_cumtrapz(psi, ylimit_min, ylimit_max, xmin, xmax,
+                         n_samples=513):
+    '''Compute the second moments (covariance matrix entries) of the
+    distribution function psi from xmin to xmax between the contours
+    ylimit_min and ylimit_max (e.g. an RFBucket separatrix) using the
+    numerical scipy.integrate.cumtrapz integration method.
+    For x the first and y the second argument of psi, return the tuple
+    (variance(x), covariance(x,y), variance(y)).
+
+    Arguments:
+        - psi: 2D distribution function with two arguments x and y
+        - ylimit_min, ylimit_max: contour functions yielding the lower
+          and the upper y(x) limit for given x
+        - xmin, xmax: lower and upper limit in the first argument of psi
+        - n_samples: integer number of sampling points for integration
     '''
 
-    x_arr = np.linspace(xmin, xmax, n_samples)
+
+    Q = compute_zero_cumtrapz(psi, ylimit_min, ylimit_max, xmin, xmax)
+    M_x = compute_mean_cumtrapz(psi, ylimit_min, ylimit_max, xmin, xmax, 'x')
+    M_y = compute_mean_cumtrapz(psi, ylimit_min, ylimit_max, xmin, xmax, 'y')
+
+    V_x = compute_var_cumtrapz(psi, ylimit_min, ylimit_max, xmin, xmax, 'x')
+    V_y = compute_var_cumtrapz(psi, ylimit_min, ylimit_max, xmin, xmax, 'y')
+
+    x_arr = np.linspace(xmin, xmax, num=n_samples)
+    dx = x_arr[1] - x_arr[0]
+
+    C_xy = 0
+    for x in x_arr:
+        y = np.linspace(ylimit_min(x), ylimit_max(x), num=n_samples)
+        z = (x - M_x) * (y - M_y) * psi(x, y)
+        C_xy += cumtrapz(z, y)[-1]
+    C_xy *= dx
+
+    C_xy /= Q
+
+    return V_x, C_xy, V_y
+
+
+### scipy.integrate.romberg standard deviation integration:
+### ==> not yet adapted to above quad and cumtrapz approaches,
+###     works as Kevin had previously defined it in the RFBucketMatcher
+###     (slightly corrected to make this function work here)
+
+def compute_std_romberg(psi, ylimits, xmin, xmax, n_samples=513):
+    '''
+    Compute the standard deviation of the distribution function psi
+    from xmin to xmax along the contours ylimits using numerical
+    integration methods.
+    '''
+
+    x_arr = np.linspace(xmin, xmax, num=n_samples)
     dx = x_arr[1] - x_arr[0]
 
     Q, V = 0, 0
     for x in x_arr:
-        y = np.linspace(0, ylimits(x), n_samples)
+        y = np.linspace(0, ylimits(x), num=n_samples)
         dy = y[1] - y[0]
         z = psi(x, y)
         Q += romb(z, dy)
