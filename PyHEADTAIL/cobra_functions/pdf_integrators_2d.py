8a3c30418d3a406433305cf71fd8b71161bf36d1
Adrian Oeftiger <oeftiger@cern.ch>
2017-06-21 11:43:53 +0200

    pdf_integrators_2d: reviving and restructuring 2D quad integrators
    Allow to compute 2D particle distribution function moments up
    to second order for a domain parametrised by
    [xmin, xmax], [ymin(x), ymax(x)].
    This is particularly useful for RFBucket matching within a
    RFBucket.interval for x and the separatrix functions for y.
    
    Started with the dblquad based integrators, cumtrapz is to come.
    So far the RFBucketMatchor still works nicely with these functions!

--- PyHEADTAIL/cobra_functions/pdf_integrators_2d.py
+++ PyHEADTAIL/cobra_functions/pdf_integrators_2d.py
@@ -8,110 +8,176 @@ def quad2d(f, ylimits, xmin, xmax):
     return Q
 
 
-def _compute_zero_quad(self, psi, p_sep, xmin, xmax):
-    '''
-    Compute the variance of the distribution function psi from xmin
-    to xmax along the contours p_sep using numerical integration
-    methods.
-    '''
+def compute_zero_quad(psi, ylimit_min, ylimit_max, xmin, xmax):
+    '''Compute the zeroth moment of the distribution function psi from
+    xmin to xmax between the contours ylimit_min and ylimit_max
+    (e.g. an RFBucket separatrix) using the numerical
+    scipy.integrate.dblquad integration method.
 
+    Arguments:
+        - psi: 2D distribution function with two arguments x and y
+        - ylimit_min, ylimit_max: contour functions yielding the lower
+          and the upper y(x) limit for given x
+        - xmin, xmax: lower and upper limit in the first argument of psi
+    '''
     Q, error = dblquad(lambda y, x: psi(x, y), xmin, xmax,
-                lambda x: 0, lambda x: p_sep(x))
+                       ylimit_min, ylimit_max)
 
     return Q
 
-def _compute_mean_quad(self, psi, p_sep, xmin, xmax):
-    '''
-    Compute the variance of the distribution function psi from xmin
-    to xmax along the contours p_sep using numerical integration
-    methods.
+def compute_mean_quad(psi, ylimit_min, ylimit_max, xmin, xmax, direction='x'):
+    '''Compute the first moment of the distribution function psi from
+    xmin to xmax between the contours ylimit_min and ylimit_max
+    (e.g. an RFBucket separatrix) using the numerical
+    scipy.integrate.dblquad integration method.
+
+    Arguments:
+        - psi: 2D distribution function with two arguments x and y
+        - ylimit_min, ylimit_max: contour functions yielding the lower
+          and the upper y(x) limit for given x
+        - xmin, xmax: lower and upper limit in the first argument of psi
+        - direction: 'x' or 'y' for calculating the mean in the first
+          or second argument of psi, respectively (default: 'x')
     '''
 
-    Q = self._compute_zero_quad(psi, p_sep, xmin, xmax)
-    M, error = dblquad(lambda y, x: x * psi(x, y), xmin, xmax,
-                lambda x: 0, lambda x: p_sep(x))
+    Q = compute_zero_quad(psi, ylimit_min, ylimit_max, xmin, xmax)
+    if direction is 'x':
+        f = lambda y, x: x * psi(x, y)
+    elif direction is 'y':
+        f = lambda y, x: y * psi(x, y)
+    else:
+        raise ValueError('direction needs to be either "x" or "y".')
+
+    M, error = dblquad(f, xmin, xmax, ylimit_min, ylimit_max)
 
     return M/Q
 
-def _compute_std_quad(self, psi, p_sep, xmin, xmax):
+def compute_var_quad(psi, ylimit_min, ylimit_max, xmin, xmax, direction='x'):
+    '''Compute the second moment (variance) with respect to the x or
+    y direction of the distribution function psi from xmin to xmax
+    between the contours ylimit_min and ylimit_max (e.g. an RFBucket
+    separatrix) using the numerical scipy.integrate.dblquad integration
+    method.
+
+    Arguments:
+        - psi: 2D distribution function with two arguments x and y
+        - ylimit_min, ylimit_max: contour functions yielding the lower
+          and the upper y(x) limit for given x
+        - xmin, xmax: lower and upper limit in the first argument of psi
+        - direction: 'x' or 'y' for calculating the standard deviation
+          in the first or second argument of psi, respectively
+          (default: 'x')
     '''
-    Compute the variance of the distribution function psi from xmin
-    to xmax along the contours p_sep using numerical integration
-    methods.
+
+    Q = compute_zero_quad(psi, ylimit_min, ylimit_max, xmin, xmax)
+    M = compute_mean_quad(psi, ylimit_min, ylimit_max, xmin, xmax, direction)
+    if direction is 'x':
+        f = lambda y, x: (x - M)**2 * psi(x, y)
+    elif direction is 'y':
+        f = lambda y, x: (y - M)**2 * psi(x, y)
+    else:
+        raise ValueError('direction needs to be either "x" or "y".')
+
+    V, error = dblquad(f, xmin, xmax, ylimit_min, ylimit_max)
+
+    return V/Q
+
+def compute_cov_quad(psi, ylimit_min, ylimit_max, xmin, xmax):
+    '''Compute the second moments (covariance matrix entries) of the
+    distribution function psi from xmin to xmax between the contours
+    ylimit_min and ylimit_max (e.g. an RFBucket separatrix) using the
+    numerical scipy.integrate.dblquad integration method.
+    For x the first and y the second argument of psi, return the tuple
+    (variance(x), covariance(x,y), variance(y)).
+
+    Arguments:
+        - psi: 2D distribution function with two arguments x and y
+        - ylimits: contour function yielding the y(x) limit for given x
+        - xmin, xmax: lower and upper limit in the first argument of psi
+
+    NB: assumes a symmetric distribution function with respect to the
+    second argument (the vertical direction).
     '''
 
-    Q = self._compute_zero_quad(psi, p_sep, xmin, xmax)
-    M = self._compute_mean_quad(psi, p_sep, xmin, xmax)
-    V, error = dblquad(lambda y, x: (x-M) ** 2 * psi(x, y), xmin, xmax,
-                       lambda x: 0, lambda x: p_sep(x))
+    Q = compute_zero_quad(psi, ylimit_min, ylimit_max, xmin, xmax)
+    M_x = compute_mean_quad(psi, ylimit_min, ylimit_max, xmin, xmax, 'x')
+    M_y = compute_mean_quad(psi, ylimit_min, ylimit_max, xmin, xmax, 'y')
 
-    return np.sqrt(V/Q)
+    V_x = compute_var_quad(psi, ylimit_min, ylimit_max, xmin, xmax, 'x')
+    V_y = compute_var_quad(psi, ylimit_min, ylimit_max, xmin, xmax, 'y')
 
-def _compute_zero_cumtrapz(self, psi, p_sep, xmin, xmax):
+    f = lambda y, x: (x - M_x) * (y - M_y) * psi(x, y)
+    C_xy, error = dblquad(f, xmin, xmax, ylimit_min, ylimit_max)
 
-    x_arr = np.linspace(xmin, xmax, 257)
+    C_xy /= Q
+
+    return V_x, C_xy, V_y
+
+def compute_zero_cumtrapz(psi, ylimits, xmin, xmax, n_samples=257):
+
+    x_arr = np.linspace(xmin, xmax, n_samples)
     dx = x_arr[1] - x_arr[0]
 
     Q = 0
     for x in x_arr:
-        y = np.linspace(0, p_sep(x), 257)
+        y = np.linspace(0, ylimits(x), n_samples)
         z = psi(x, y)
         Q += cumtrapz(z, y)[-1]
     Q *= dx
 
     return Q
 
-def _compute_mean_cumtrapz(self, psi, p_sep, xmin, xmax):
+def compute_mean_cumtrapz(psi, ylimits, xmin, xmax, n_samples=257):
 
-    Q = self._compute_zero_cumtrapz(psi, p_sep, xmin, xmax)
+    Q = compute_zero_cumtrapz(psi, ylimits, xmin, xmax, n_samples)
 
-    x_arr = np.linspace(xmin, xmax, 257)
+    x_arr = np.linspace(xmin, xmax, n_samples)
     dx = x_arr[1] - x_arr[0]
 
     M = 0
     for x in x_arr:
-        y = np.linspace(0, p_sep(x), 257)
+        y = np.linspace(0, ylimits(x), n_samples)
         z = x * psi(x, y)
         M += cumtrapz(z, y)[-1]
     M *= dx
 
     return M/Q
 
-def _compute_std_cumtrapz(self, psi, p_sep, xmin, xmax):
+def compute_std_cumtrapz(psi, ylimits, xmin, xmax, n_samples=257):
     '''
     Compute the variance of the distribution function psi from xmin
-    to xmax along the contours p_sep using numerical integration
+    to xmax along the contours ylimits using numerical integration
     methods.
     '''
 
-    Q = self._compute_zero_cumtrapz(psi, p_sep, xmin, xmax)
-    M = self._compute_mean_cumtrapz(psi, p_sep, xmin, xmax)
+    Q = compute_zero_cumtrapz(psi, ylimits, xmin, xmax, n_samples)
+    M = compute_mean_cumtrapz(psi, ylimits, xmin, xmax, n_samples)
 
-    x_arr = np.linspace(xmin, xmax, 257)
+    x_arr = np.linspace(xmin, xmax, n_samples)
     dx = x_arr[1] - x_arr[0]
 
     V = 0
     for x in x_arr:
-        y = np.linspace(0, p_sep(x), 257)
+        y = np.linspace(0, ylimits(x), n_samples)
         z = (x-M)**2 * psi(x, y)
         V += cumtrapz(z, y)[-1]
     V *= dx
 
     return np.sqrt(V/Q)
 
-def _compute_std_romberg(self, psi, p_sep, xmin, xmax):
+def compute_std_romberg(psi, ylimits, xmin, xmax, n_samples=257):
     '''
     Compute the variance of the distribution function psi from xmin
-    to xmax along the contours p_sep using numerical integration
+    to xmax along the contours ylimits using numerical integration
     methods.
     '''
 
-    x_arr = np.linspace(xmin, xmax, 257)
+    x_arr = np.linspace(xmin, xmax, n_samples)
     dx = x_arr[1] - x_arr[0]
 
     Q, V = 0, 0
     for x in x_arr:
-        y = np.linspace(0, p_sep(x), 257)
+        y = np.linspace(0, ylimits(x), n_samples)
         dy = y[1] - y[0]
         z = psi(x, y)
         Q += romb(z, dy)
