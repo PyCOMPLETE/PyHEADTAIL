818b8480f940111bffca771cadadd6d69950e73c
lmether <lotta.mether@cern.ch>
2019-11-06 10:21:46 +0100

    Added feedback module from multi-bunch branch.

--- PyHEADTAIL/feedback/__init__.py
+++ PyHEADTAIL/feedback/__init__.py
@@ -1,2 +1,4 @@
 from .. import __version__
 from .. import Element
+from core import version
+print ('PyHEADTAIL_feedback v' + version + '\n\n')
--- setup.py
+++ setup.py
@@ -77,7 +77,11 @@ cy_ext = [
               ["PyHEADTAIL/cobra_functions/interp_sin_cos.pyx"],
               include_dirs=[np.get_include()],
               library_dirs=[], libraries=["m"],
-              extra_compile_args=["-fopenmp"], extra_link_args=["-fopenmp"])
+              extra_compile_args=["-fopenmp"], extra_link_args=["-fopenmp"]),
+    Extension("PyHEADTAIL.feedback.processors.cython_hacks",
+              ["PyHEADTAIL/feedback/processors/cython_hacks.pyx"],
+              include_dirs=[np.get_include()],
+              library_dirs=[], libraries=["m"])
 ]
 
 setup(
