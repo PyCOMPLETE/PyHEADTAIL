695be4af753e4f23f0cdbee97fe18f99c244b7c0
giadarol <giovanni.iadarola@cern.ch>
2019-12-05 15:34:20 +0100

    Change extensions

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -29,8 +29,10 @@ author = 'Author'
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.todo',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.coverage'
+    #'sphinx.ext.viewcode',
+    #'sphinx.ext.todo',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
