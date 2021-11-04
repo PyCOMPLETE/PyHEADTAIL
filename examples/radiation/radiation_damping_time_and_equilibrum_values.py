d3ffcddcc6819f5fa0691734aa25952f7289411e
lmether <lotta.mether@cern.ch>
2021-11-04 18:24:37 +0100

    Improved readability and output in radiation examples.

--- examples/radiation/radiation_damping_time_and_equilibrum_values.py
+++ examples/radiation/radiation_damping_time_and_equilibrum_values.py
@@ -1,12 +1,13 @@
 import sys
+
 import matplotlib.pyplot as plt
 import numpy as np
-import pickle
-from scipy.constants import e,c
+from scipy.constants import c
 
+from CLIC_DR import CLIC_DR
 from PyHEADTAIL.radiation.radiation import SynchrotronRadiationTransverse
 from PyHEADTAIL.radiation.radiation import SynchrotronRadiationLongitudinal
-from CLIC_DR import CLIC_DR
+
 
 macroparticlenumber = 50000
 n_turns = 512*8
@@ -36,15 +37,24 @@ bunch.yp += 1.e-6
 damping_time_x_turns = (2e-3*(c/machine.circumference))
 damping_time_y_turns = (2e-3*(c/machine.circumference))
 damping_time_z_turns = (1e-3*(c/machine.circumference))
+
 E_loss_eV = 3.98e6
-eq_emit_x=0.456e-6
-eq_emit_y=0.0048e-6
-eq_sig_dp=1.074e-3
-SynchrotronRadiationTransverse = SynchrotronRadiationTransverse(eq_emit_x=eq_emit_x, eq_emit_y=eq_emit_y, 
-	damping_time_x_turns=damping_time_x_turns, damping_time_y_turns=damping_time_y_turns, beta_x=machine.transverse_map.beta_x[-1], 
-	beta_y=machine.transverse_map.beta_y[-1])
+eq_emit_x = 0.456e-6
+eq_emit_y = 0.0048e-6
+eq_sig_dp = 1.074e-3
+
+SynchrotronRadiationTransverse = SynchrotronRadiationTransverse(
+    eq_emit_x=eq_emit_x, eq_emit_y=eq_emit_y,
+    damping_time_x_turns=damping_time_x_turns,
+    damping_time_y_turns=damping_time_y_turns,
+    beta_x=machine.transverse_map.beta_x[-1],
+    beta_y=machine.transverse_map.beta_y[-1]
+)
 SynchrotronRadiationLongitudinal = SynchrotronRadiationLongitudinal(
-	eq_sig_dp=eq_sig_dp, damping_time_z_turns=damping_time_z_turns, E_loss_eV=E_loss_eV)
+    eq_sig_dp=eq_sig_dp,
+    damping_time_z_turns=damping_time_z_turns,
+    E_loss_eV=E_loss_eV
+)
 
 # TRACKING LOOP
 # =============
@@ -58,7 +68,9 @@ mean_dp = []
 sx, sy, sz, sdp = [], [], [], []
 epsx, epsy, epsz = [], [], []
 for i_turn in range(n_turns):
-    print('Turn %d/%d'%(i_turn, n_turns))
+
+    if i_turn % 100 == 0:
+        print('Turn %d/%d'%(i_turn, n_turns))
     machine.track(bunch)
 
     beam_x.append(bunch.mean_x())
@@ -76,20 +88,31 @@ for i_turn in range(n_turns):
 # PARAMETER EVALUATION
 # ====================
 
-n_turns_damping = int(2*damping_time_x_turns) #number of turns to evaluate the damping time
-n_intervals_damping = 10             #number of intervals to evaluate the damping time
-interval_vector_damping = np.arange(0, (n_turns_damping), (n_turns_damping/n_intervals_damping))
+# number of turns to evaluate the damping time
+n_turns_damping = int(2*damping_time_x_turns)
+# number of intervals to evaluate the damping time
+n_intervals_damping = 10
+interval_vector_damping = np.arange(0, (n_turns_damping),
+                                    (n_turns_damping/n_intervals_damping))
 N_interval_damping = len(interval_vector_damping)
+
 xx_x = beam_x
 xx_y = beam_y
 xx_x_env_damping = []
 xx_y_env_damping = []
-for j in range(N_interval_damping): #for loop to evaluate the envelope of centroid position in x and y
-    xx_x_env_damping.append(np.max(xx_x[((n_turns_damping//n_intervals_damping)*j):((n_turns_damping//n_intervals_damping)*(j+1))])) # take the maximum in each interval
-    xx_y_env_damping.append(np.max(xx_y[((n_turns_damping//n_intervals_damping)*j):((n_turns_damping//n_intervals_damping)*(j+1))]))
-xx_x_env_log_damping = np.log(np.abs(xx_x_env_damping) + sys.float_info.epsilon) #exponential fit using linear fit (exp->log)
-p0_x, p1_x = np.polyfit(interval_vector_damping, xx_x_env_log_damping, 1)
+
+# for loop to evaluate the envelope of centroid position in x and y
+for j in range(N_interval_damping):
+    i_start = (n_turns_damping // n_intervals_damping) * j
+    i_end = (n_turns_damping // n_intervals_damping) * (j+1)
+    # take the maximum in each interval
+    xx_x_env_damping.append(np.max(xx_x[i_start:i_end]))
+    xx_y_env_damping.append(np.max(xx_y[i_start:i_end]))
+
+# exponential fit using linear fit (exp->log)
+xx_x_env_log_damping = np.log(np.abs(xx_x_env_damping) + sys.float_info.epsilon)
 xx_y_env_log_damping = np.log(np.abs(xx_y_env_damping) + sys.float_info.epsilon)
+p0_x, p1_x = np.polyfit(interval_vector_damping, xx_x_env_log_damping, 1)
 p0_y, p1_y = np.polyfit(interval_vector_damping, xx_y_env_log_damping, 1)
 
 tt = np.arange(n_turns)
@@ -99,49 +122,94 @@ p0_z, p1_z = np.polyfit(tt, xx_z, 1)
 
 # PLOT
 # ====
+damping_time_x = damping_time_x_turns * (machine.circumference/c)
+eval_damping_time_x = (-1/p0_x) * (machine.circumference/c)
+eval_error_x = np.abs(damping_time_x_turns - (-1/p0_x)) * 100 / damping_time_x_turns
+
+damping_time_y = damping_time_y_turns * (machine.circumference/c)
+eval_damping_time_y = (-1/p0_y) * (machine.circumference/c)
+eval_error_y = np.abs(damping_time_y_turns - (-1/p0_y)) * 100 / damping_time_y_turns
+
+damping_time_z = damping_time_z_turns * (machine.circumference/c)
+eval_damping_time_z = (-1/p0_z) * (machine.circumference/c)
+eval_error_z = np.abs(damping_time_z_turns - (-1/p0_z)) * 100 / damping_time_z_turns
+
+epsx_error = np.abs(eq_emit_x - epsx[-1]) * 100 / eq_emit_x
+epsy_error = np.abs(eq_emit_y - epsy[-1]) * 100 / eq_emit_y
+sdp_error = np.abs(eq_sig_dp - sdp[-1]) * 100 / eq_sig_dp
 
 plt.figure(1, figsize=(16, 8), tight_layout=True)
-plt.subplot(2,3,1)
+
+plt.subplot(2, 3, 1)
 plt.plot(beam_x)
-plt.plot(np.max(beam_x)*np.exp(-tt/damping_time_x_turns), label= 'Damping time\nExpected :%.2e [s]'%(damping_time_x_turns*(machine.circumference/c)), lw=2, color = 'black')
-plt.plot(np.exp(p1_x + tt * p0_x), label= 'Evaluated :%.2e [s]\nERROR :%.2f'%((-1/p0_x)*(machine.circumference/c),np.abs((damping_time_x_turns)-(-1/p0_x))*100/(damping_time_x_turns))+'%', lw=2, color = 'red', linestyle = '--')
+plt.plot(np.max(beam_x)*np.exp(-tt/damping_time_x_turns),
+         label=f'Damping time\nExpected {damping_time_x:.2e} [s]',
+         lw=2, color = 'black')
+plt.plot(np.exp(p1_x + tt * p0_x),
+         label=f'Evaluated {eval_damping_time_x:.2e} [s]\nERROR {eval_error_x:.2f}%',
+         lw=2, color = 'red', linestyle = '--')
 plt.legend (loc=0, fontsize = 10)
-plt.ylabel('x [m]');plt.xlabel('Turn')
-plt.gca().ticklabel_format(style='sci', scilimits=(0,0),axis='y')
-plt.subplot(2,3,2)
+plt.ylabel('x [m]')
+plt.xlabel('Turn')
+plt.gca().ticklabel_format(style='sci', scilimits=(0, 0), axis='y')
+
+plt.subplot(2, 3, 2)
 plt.plot(beam_y)
-plt.plot(np.max(beam_y)*np.exp(-tt/damping_time_y_turns), label= 'Damping time\nExpected :%.2e [s]'%(damping_time_y_turns*(machine.circumference/c)), lw=2, color = 'black')
-plt.plot(np.exp(p1_y + tt * p0_y), label= 'Evaluated :%.2e [s]\nERROR :%.2f'%((-1/p0_y)*(machine.circumference/c),np.abs((damping_time_y_turns)-(-1/p0_y))*100/(damping_time_y_turns))+'%', lw=2, color = 'red', linestyle = '--')
+plt.plot(np.max(beam_y)*np.exp(-tt/damping_time_y_turns),
+         label=f'Damping time\nExpected {damping_time_y:.2e} [s]',
+         lw=2, color = 'black')
+plt.plot(np.exp(p1_y + tt * p0_y),
+         label=f'Evaluated {eval_damping_time_y:.2e} [s]\nERROR {eval_error_y:.2f}%',
+         lw=2, color = 'red', linestyle = '--')
 plt.legend (loc=0, fontsize = 10)
-plt.ylabel('y [m]');plt.xlabel('Turn')
-plt.gca().ticklabel_format(style='sci', scilimits=(0,0),axis='y')
-plt.subplot(2,3,3)
+plt.ylabel('y [m]')
+plt.xlabel('Turn')
+plt.gca().ticklabel_format(style='sci', scilimits=(0, 0), axis='y')
+
+plt.subplot(2, 3, 3)
 plt.plot(beam_z)
-plt.plot((np.max(beam_z)-beam_z[-1])*np.exp(-tt/damping_time_z_turns)+beam_z[-1], label= 'Damping time\nExpected :%.2e [s]'%(damping_time_z_turns*(machine.circumference/c)), lw=2, color = 'black')
-plt.plot(2*np.exp(p1_z + tt * p0_z)+ beam_z[-1], label= 'Evaluated :%.2e [s]\nERROR :%.2f'%((-1/p0_z)*(machine.circumference/c),np.abs((damping_time_z_turns)-(-1/p0_z))*100/(damping_time_z_turns))+'%', lw=2, color = 'red', linestyle = '--')
+plt.plot((np.max(beam_z)-beam_z[-1])*np.exp(-tt/damping_time_z_turns)+beam_z[-1],
+         label=f'Damping time\nExpected {damping_time_z:.2e} [s]',
+         lw=2, color = 'black')
+plt.plot(2*np.exp(p1_z + tt * p0_z) + beam_z[-1],
+         label=f'Evaluated {eval_damping_time_z:.2e} [s]\nERROR {eval_error_z:.2f}%',
+         lw=2, color = 'red', linestyle = '--')
 plt.legend (loc=0, fontsize = 10)
-plt.ylabel('z [m]');plt.xlabel('Turn')
-plt.gca().ticklabel_format(style='sci', scilimits=(0,0),axis='y')
-plt.subplot(2,3,4)
+plt.ylabel('z [m]')
+plt.xlabel('Turn')
+plt.gca().ticklabel_format(style='sci', scilimits=(0, 0), axis='y')
+
+plt.subplot(2, 3, 4)
 plt.plot(epsx)
-plt.axhline(eq_emit_x, label= 'Equilibrium emittance\nExpected :%.2e [s]'%(eq_emit_x), lw=2, color = 'black')
-plt.axhline(epsx[-1], label= 'Evaluated :%.2e [s]\nERROR :%.2f'%(epsx[-1],np.abs(eq_emit_x-epsx[-1])*100/eq_emit_x)+'%', lw=2, color = 'red', linestyle = '--')
+plt.axhline(eq_emit_x, label=f'Equilibrium emittance\nExpected {eq_emit_x:.2e} [s]',
+            lw=2, color = 'black')
+plt.axhline(epsx[-1], label=f'Evaluated {epsx[-1]:.2e} [s]\nERROR {epsx_error:.2f}%',
+            lw=2, color = 'red', linestyle = '--')
 plt.legend (loc=0, fontsize = 10)
-plt.ticklabel_format(useOffset=False, style='sci', scilimits=(0,0),axis='y')
-plt.ylabel('$\epsilon_x$ [m.rad]');plt.xlabel('Turn')
-plt.subplot(2,3,5)
+plt.ticklabel_format(useOffset=False, style='sci', scilimits=(0, 0), axis='y')
+plt.ylabel('$\epsilon_x$ [m.rad]')
+plt.xlabel('Turn')
+
+plt.subplot(2, 3, 5)
 plt.plot(epsy)
-plt.axhline(eq_emit_y, label= 'Equilibrium emittance\nExpected :%.2e [s]'%(eq_emit_y), lw=2, color = 'black')
-plt.axhline(epsy[-1], label= 'Evaluated :%.2e [s]\nERROR :%.2f'%(epsy[-1],np.abs(eq_emit_y-epsy[-1])*100/eq_emit_y)+'%', lw=2, color = 'red', linestyle = '--')
+plt.axhline(eq_emit_y, label=f'Equilibrium emittance\nExpected {eq_emit_y:.2e} [s]',
+            lw=2, color = 'black')
+plt.axhline(epsy[-1], label=f'Evaluated {epsy[-1]:.2e} [s]\nERROR {epsy_error:.2f}%',
+            lw=2, color = 'red', linestyle = '--')
 plt.legend (loc=0, fontsize = 10)
-plt.ticklabel_format(useOffset=False, style='sci', scilimits=(0,0),axis='y')
-plt.ylabel('$\epsilon_y$ [m.rad]');plt.xlabel('Turn')
-plt.subplot(2,3,6)
+plt.ticklabel_format(useOffset=False, style='sci', scilimits=(0, 0), axis='y')
+plt.ylabel('$\epsilon_y$ [m.rad]')
+plt.xlabel('Turn')
+
+plt.subplot(2, 3, 6)
 plt.plot(sdp)
-plt.axhline(eq_sig_dp, label= 'Equilibrium momentum spread\nExpected :%.2e'%(eq_sig_dp), lw=2, color = 'black')
-plt.axhline(sdp[-1], label= 'Evaluated :%.2e\nERROR :%.2f'%(sdp[-1],np.abs(eq_sig_dp-sdp[-1])*100/eq_sig_dp)+'%', lw=2, color = 'red', linestyle = '--')
+plt.axhline(eq_sig_dp, label=f'Equilibrium momentum spread\nExpected {eq_sig_dp:.2e}',
+            lw=2, color = 'black')
+plt.axhline(sdp[-1], label=f'Evaluated {sdp[-1]:.2e} [s]\nERROR {sdp_error:.2f}%',
+            lw=2, color = 'red', linestyle = '--')
 plt.legend (loc=0, fontsize = 10)
-plt.ticklabel_format(useOffset=False, style='sci', scilimits=(0,0),axis='y')
-plt.ylabel('$\sigma_{dp}$');plt.xlabel('Turn')
+plt.ticklabel_format(useOffset=False, style='sci', scilimits=(0, 0), axis='y')
+plt.ylabel('$\sigma_{dp}$')
+plt.xlabel('Turn')
 
 plt.show()
--- examples/radiation/radiation_with_non_linear_bucket.py
+++ examples/radiation/radiation_with_non_linear_bucket.py
@@ -1,14 +1,10 @@
-
-
 import matplotlib.pyplot as plt
 import numpy as np
-import pickle
-import time
-from scipy.constants import e,c
+from scipy.constants import c
 
-from PyHEADTAIL.radiation.radiation import SynchrotronRadiationTransverse
-from PyHEADTAIL.radiation.radiation import SynchrotronRadiationLongitudinal
 from CLIC_DR import CLIC_DR
+from PyHEADTAIL.radiation.radiation import SynchrotronRadiationLongitudinal
+
 
 macroparticlenumber = 50000
 n_turns = 512*8
@@ -21,37 +17,42 @@ n_turns_vec = np.arange(0, n_turns, 1)*(machine.circumference/c)*1e3
 # =====================
 damping_time_z_turns = (1e-3*(c/machine.circumference))
 E_loss_eV = 3.98e6
-eq_sig_dp=1.074e-3
+eq_sig_dp = 1.074e-3
 SynchrotronRadiationLongitudinal = SynchrotronRadiationLongitudinal(
-    eq_sig_dp=eq_sig_dp, damping_time_z_turns=damping_time_z_turns, E_loss_eV=E_loss_eV)
+    eq_sig_dp=eq_sig_dp,
+    damping_time_z_turns=damping_time_z_turns,
+    E_loss_eV=E_loss_eV
+)
 
 # EVALUATE STABLE PHASE AND Z
 # ===========================
 phi_s = np.arcsin(E_loss_eV/machine.longitudinal_map.voltages[0])
-z_s = (machine.circumference)*phi_s/(2*np.pi*machine.longitudinal_map.harmonics[0])
+z_s = machine.circumference * phi_s / (2*np.pi*machine.longitudinal_map.harmonics[0])
 print(z_s)
 
 # BEAM
 # ====
-epsn_x  = 5*0.456e-6
-epsn_y  = 5*0.0048e-6
+epsn_x = 5*0.456e-6
+epsn_y = 5*0.0048e-6
 sigma_z = 1.8e-3
 
 intensity = 4.1e9
-bunch   = machine.generate_6D_Gaussian_bunch(
-    macroparticlenumber, intensity, epsn_x, epsn_y, sigma_z=sigma_z)
+bunch = machine.generate_6D_Gaussian_bunch(
+    macroparticlenumber, intensity,
+    epsn_x, epsn_y, sigma_z=sigma_z
+)
 
-bunch.x  += 1.e-6
+bunch.x += 1.e-6
 bunch.xp += 1.e-6
-bunch.y  += 1.e-6
+bunch.y += 1.e-6
 bunch.yp += 1.e-6
 # bunch.z  *= 0.
 bunch.dp *= 0.
-bunch.z  += z_s
+bunch.z += z_s
 
 # TRACKING LOOP
 # =============
-machine.one_turn_map = [SynchrotronRadiationLongitudinal]+machine.one_turn_map
+machine.one_turn_map = [SynchrotronRadiationLongitudinal] + machine.one_turn_map
 
 # Tracking
 # ========
@@ -60,26 +61,27 @@ plt.ion()
 
 print('--> Begin tracking...')
 
-fig, ((ax1,ax2),(ax3,ax4)) = plt.subplots(2, 2, figsize=(14,10))
+fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, figsize=(14, 10))
 
-sigma_x  = np.zeros(n_turns) + np.NAN
+sigma_x = np.zeros(n_turns) + np.NAN
 sigma_xp = np.zeros(n_turns) + np.NAN
-mean_x   = np.zeros(n_turns) + np.NAN
-epsn_x   = np.zeros(n_turns) + np.NAN
-sigma_y  = np.zeros(n_turns) + np.NAN
+mean_x = np.zeros(n_turns) + np.NAN
+epsn_x = np.zeros(n_turns) + np.NAN
+sigma_y = np.zeros(n_turns) + np.NAN
 sigma_yp = np.zeros(n_turns) + np.NAN
-mean_y   = np.zeros(n_turns) + np.NAN
-epsn_y   = np.zeros(n_turns) + np.NAN
-sigma_z  = np.zeros(n_turns) + np.NAN
+mean_y = np.zeros(n_turns) + np.NAN
+epsn_y = np.zeros(n_turns) + np.NAN
+sigma_z = np.zeros(n_turns) + np.NAN
 sigma_dp = np.zeros(n_turns) + np.NAN
-mean_z   = np.zeros(n_turns) + np.NAN
-epsn_z   = np.zeros(n_turns) + np.NAN
-mean_dp   = np.zeros(n_turns) + np.NAN
+mean_z = np.zeros(n_turns) + np.NAN
+epsn_z = np.zeros(n_turns) + np.NAN
+mean_dp = np.zeros(n_turns) + np.NAN
 
 for i in range(n_turns):
 
     machine.track(bunch)
-    print('Turn %d/%d'%(i, n_turns))
+    if i % 100 == 0:
+        print('Turn %d/%d'%(i, n_turns))
     sigma_x[i]  = bunch.sigma_x()
     mean_x[i]   = bunch.mean_x()
     epsn_x[i]   = bunch.epsn_x()
@@ -92,7 +94,7 @@ for i in range(n_turns):
     epsn_z[i]   = bunch.epsn_z()
     mean_dp[i]  = bunch.mean_dp()
 
-    if not (i+1)%100:
+    if not (i+1) % 100:
         ax1.cla()
         ax2.cla()
         ax3.cla()
@@ -109,7 +111,7 @@ for i in range(n_turns):
         ax.scatter(bunch.z, bunch.dp, s=8, color = 'b')
         ax.scatter(bunch.z[50], bunch.dp[50], s=8, color = 'r')
         ax.scatter(bunch.z[51], bunch.dp[51], s=8, color = 'w')
-        ax.ticklabel_format(style='sci', scilimits=(0,0),axis='both', useOffset = False)
+        ax.ticklabel_format(style='sci', scilimits=(0, 0), axis='both', useOffset=False)
 
         ax = ax2
         plt.rc("font", size=size_font)
@@ -118,7 +120,7 @@ for i in range(n_turns):
         ax.plot(n_turns_vec, mean_z)
         ax.axhline(z_s, label= 'Equilibrium z\nExpected :%.2e [m]'%(z_s), lw=2, color = 'red')
         ax.set_ylabel('$<z>$ [m]', fontsize=size_font)
-        ax.ticklabel_format(style='sci', scilimits=(0,0),axis='y', useOffset = False)
+        ax.ticklabel_format(style='sci', scilimits=(0, 0), axis='y', useOffset=False)
         ax.legend (loc=0, fontsize = 10)
 
         ax = ax3
@@ -128,33 +130,36 @@ for i in range(n_turns):
         ax.plot(n_turns_vec, mean_dp)
         ax.set_ylabel('mean_dp', fontsize=size_font)
         ax.axhline(E_loss_eV/(machine.p0*c/np.abs(machine.charge))/2.,
-        label= 'Equilibrium dp\nExpected :%.2e'%(E_loss_eV/(machine.p0*c/np.abs(machine.charge))/2.), lw=2, color = 'red')
-        ax.ticklabel_format(style='sci', scilimits=(0,0), axis='y', useOffset = False)  
+                   label='Equilibrium dp\nExpected :%.2e'%(
+                       E_loss_eV/(machine.p0*c/np.abs(machine.charge))/2.
+                   ), lw=2, color = 'red')
+        ax.ticklabel_format(style='sci', scilimits=(0, 0), axis='y', useOffset=False)
         ax.legend (loc=0, fontsize = 10)
 
         ax = ax4
         plt.rc("font", size=size_font)
         ax.set_xlim(0, n_turns_vec[-1])
         ax.set_xlabel('t [ms]', fontsize=size_font)
-        ax.axhline(eq_sig_dp, label= 'Equilibrium momentum spread\nExpected :%.2e'%(eq_sig_dp), lw=2, color = 'red')
+        ax.axhline(eq_sig_dp,
+                   label='Equilibrium momentum spread\nExpected :%.2e'%(eq_sig_dp),
+                   lw=2, color = 'red')
         ax.plot(n_turns_vec, sigma_dp)
         ax.legend (loc=0, fontsize = 10)
         ax.set_ylabel('$\sigma_{dp}$', fontsize=size_font)
-        ax.ticklabel_format(style='sci', scilimits=(0,0), axis='y', useOffset = False)
+        ax.ticklabel_format(style='sci', scilimits=(0,0), axis='y', useOffset=False)
 
-        plt.suptitle('Longitudinal phase space is samples "opposite" to the kicks (half drift occurred).')
+        plt.suptitle('Longitudinal phase space is sampled "opposite" to the kicks (half drift occurred).')
 
-        #plt.savefig('mismatched_nonlinear_' + '%02d'%i + '.png', dpi=150)
+        # plt.savefig('mismatched_nonlinear_' + '%02d'%i + '.png', dpi=150)
 
         plt.pause(.001)
         plt.draw()
 
-
         ''
-        #print 'bunch.z and bunch.dp = ',bunch.z, bunch.dp
-        #print 'mean_z = ',mean_z
-        #print 'sigma_dp = ',sigma_dp
-        #print 'sigma_z = ',sigma_z
+        # print 'bunch.z and bunch.dp = ',bunch.z, bunch.dp
+        # print 'mean_z = ',mean_z
+        # print 'sigma_dp = ',sigma_dp
+        # print 'sigma_z = ',sigma_z
         
 print('--> Done.')
 
