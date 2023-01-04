9bf7fd581e5d1aaf510f37ba4f460f591d0f91be
gubaidulinvadim <gubaidulinvadim@gmail.com>
2023-01-04 09:34:13 +0100

    - distinction between weak strong models - ions are generated in balanced pairs

--- PyHEADTAIL/ion_cloud/ion_cloud.py
+++ PyHEADTAIL/ion_cloud/ion_cloud.py
@@ -24,7 +24,8 @@ class BeamIonElement(Element):
                  set_aperture=True,
                  n_segments=500,
                  circumference=354,
-                 n_steps=None
+                 n_steps=None,
+                 interaction_model='weak-weak'
                  ):
         self.ion_beam = None
         self.dist = dist_ions
@@ -40,6 +41,7 @@ class BeamIonElement(Element):
             self.dist_func_y = generators.uniform2D
         else:
             print('Distribution given is not implemented')
+        self.interaction_model = interaction_model
         self.set_aperture = set_aperture
         self.L_sep = L_sep
         self.N_MACROPARTICLES = 30
@@ -105,7 +107,7 @@ class BeamIonElement(Element):
         '''
         ION_INTENSITY_PER_ELECTRON_BUNCH = electron_bunch.intensity * \
             self.sigma_i*self.n_g*self.L_SEG
-        self.N_MACROPARTICLES = int(ION_INTENSITY_PER_ELECTRON_BUNCH)
+        self.N_MACROPARTICLES = 50  # int(ION_INTENSITY_PER_ELECTRON_BUNCH)
         assert (self.dist in ['LN', 'GS']), (
             'The implementation for required distribution {:} is not found'.format(self.dist))
         if self.dist == 'LN':
@@ -114,10 +116,14 @@ class BeamIonElement(Element):
         elif self.dist == 'GS':
             a_x, b_x = electron_bunch.sigma_x(), electron_bunch.sigma_xp()
             a_y, b_y = electron_bunch.sigma_y(), electron_bunch.sigma_yp()
+        '''
+        Particles are generated in pairs -x, -y and +x, +y to avoid numerical noise.
+        The idea came from Blaskiewicz, M. (2019) https://doi.org/10.18429/JACoW-NAPAC2019-TUPLM11
+        '''
         if self.ion_beam.macroparticlenumber < self.N_MACROPARTICLES_MAX:
             new_particles = generators.ParticleGenerator(
-                macroparticlenumber=self.N_MACROPARTICLES,
-                intensity=ION_INTENSITY_PER_ELECTRON_BUNCH,
+                macroparticlenumber=self.N_MACROPARTICLES//2,
+                intensity=ION_INTENSITY_PER_ELECTRON_BUNCH//2,
                 charge=self.charge_state*e,
                 gamma=1.0001,
                 mass=self.A*m_p,
@@ -130,6 +136,25 @@ class BeamIonElement(Element):
                 limit_n_rms_y=3.,
                 printer=SilentPrinter()
             ).generate()
+            new_particles_twin = particles.Particles(
+                macroparticlenumber=self.N_MACROPARTICLES//2,
+                particlenumber_per_mp=ION_INTENSITY_PER_ELECTRON_BUNCH/self.N_MACROPARTICLES,
+                charge=self.charge_state*e,
+                gamma=1.0001,
+                mass=self.A*m_p,
+                circumference=self.CIRCUMFERENCE,
+                coords_n_momenta_dict={
+                    'x': -new_particles.x,
+                    'xp': -new_particles.xp,
+                    'y': -new_particles.y,
+                    'yp': -new_particles.yp,
+                    'z': -new_particles.z,
+                    'dp': -new_particles.dp
+                },
+                printer=SilentPrinter()
+            )
+            new_particles += new_particles_twin
+            # Initial conditions
             new_particles.x[:] += electron_bunch.mean_x()
             new_particles.y[:] += electron_bunch.mean_y()
             new_particles.xp[:] = 0
@@ -137,34 +162,66 @@ class BeamIonElement(Element):
             self.ion_beam += new_particles
         else:
             self.ion_beam.intensity += ION_INTENSITY_PER_ELECTRON_BUNCH
+        self.ions_monitor.dump(self.ion_beam)
         prefactor_kick_ion_field = -(self.ion_beam.intensity *
-                                     self.ion_beam.charge*electron_bunch.charge*electron_bunch.gamma /
+                                     self.ion_beam.charge*electron_bunch.charge /
                                      (electron_bunch.p0*electron_bunch.beta*c))*self.L_SEG/self.L_sep
         prefactor_kick_electron_field = -(electron_bunch.intensity *
                                           electron_bunch.charge*self.ion_beam.charge /
-                                          c)
+                                          (self.ion_beam.mass*c**2))
+        # Clearing ions that are far away
         apt_xy = aperture.EllipticalApertureXY(
-            x_aper=5*electron_bunch.sigma_x(), y_aper=5*electron_bunch.sigma_y())
+            x_aper=5*electron_bunch.sigma_x(),
+            y_aper=5*electron_bunch.sigma_y())
         if self.set_aperture == True:
             apt_xy.track(self.ion_beam)
-        # else:
-        # pass
+        else:
+            pass
         p_id_electrons = electron_bunch.id-1
         p_id_ions = linspace(
             0, self.ion_beam.y.shape[0]-1, self.ion_beam.y.shape[0], dtype=int64)
 # Electric field of ions
-        en_ions_x, en_ions_y = self.get_efieldn(
-            pm.take(electron_bunch.x, p_id_electrons),
-            pm.take(electron_bunch.y, p_id_electrons),
-            self.ion_beam.mean_x(), self.ion_beam.mean_y(),
-            self.ion_beam.sigma_x(), self.ion_beam.sigma_y())
+        if self.interaction_model == 'weak-weak':
+            en_ions_x, en_ions_y = self.get_efieldn(
+                pm.take(electron_bunch.x, p_id_electrons),
+                pm.take(electron_bunch.y, p_id_electrons),
+                self.ion_beam.mean_x(), self.ion_beam.mean_y(),
+                self.ion_beam.sigma_x(), self.ion_beam.sigma_y())
 # Electric field of electrons
-        en_electrons_x, en_electrons_y = self.get_efieldn(
-            pm.take(self.ion_beam.x, p_id_ions),
-            pm.take(self.ion_beam.y, p_id_ions),
-            electron_bunch.mean_x(),  electron_bunch.mean_y(),
-            electron_bunch.sigma_x(), electron_bunch.sigma_y()
-        )
+            en_electrons_x, en_electrons_y = self.get_efieldn(
+                pm.take(self.ion_beam.x, p_id_ions),
+                pm.take(self.ion_beam.y, p_id_ions),
+                electron_bunch.mean_x(),  electron_bunch.mean_y(),
+                electron_bunch.sigma_x(), electron_bunch.sigma_y()
+            )
+        elif self.interaction_model == 'weak-strong':
+            en_ions_x, en_ions_y = self.get_efieldn(
+                electron_bunch.mean_x(),
+                electron_bunch.mean_y(),
+                self.ion_beam.mean_x(), self.ion_beam.mean_y(),
+                self.ion_beam.sigma_x(), self.ion_beam.sigma_y())
+# Electric field of electrons
+            en_electrons_x, en_electrons_y = self.get_efieldn(
+                pm.take(self.ion_beam.x, p_id_ions),
+                pm.take(self.ion_beam.y, p_id_ions),
+                electron_bunch.mean_x(),  electron_bunch.mean_y(),
+                electron_bunch.sigma_x(), electron_bunch.sigma_y()
+            )
+        elif self.interaction_model == 'strong-strong':
+            en_ions_x, en_ions_y = self.get_efieldn(
+                electron_bunch.mean_x(),
+                electron_bunch.mean_y(),
+                self.ion_beam.mean_x(), self.ion_beam.mean_y(),
+                self.ion_beam.sigma_x(), self.ion_beam.sigma_y())
+# Electric field of electrons
+            en_electrons_x, en_electrons_y = self.get_efieldn(
+                self.ion_beam.mean_x(),
+                self.ion_beam.mean_x(),
+                electron_bunch.mean_x(),  electron_bunch.mean_y(),
+                electron_bunch.sigma_x(), electron_bunch.sigma_y()
+            )
+        else:
+            pass
         kicks_electrons_x = en_ions_x * prefactor_kick_ion_field
         kicks_electrons_y = en_ions_y * prefactor_kick_ion_field
         kicks_ions_x = en_electrons_x * prefactor_kick_electron_field
@@ -173,7 +230,6 @@ class BeamIonElement(Element):
             electron_bunch.xp, p_id_electrons) + kicks_electrons_x
         kicked_electrons_yp = pm.take(
             electron_bunch.yp, p_id_electrons) + kicks_electrons_y
-        # print(kicks_electrons_y)
 
         kicked_ions_xp = pm.take(self.ion_beam.xp, p_id_ions) + kicks_ions_x
         kicked_ions_yp = pm.take(self.ion_beam.yp, p_id_ions) + kicks_ions_y
@@ -184,13 +240,12 @@ class BeamIonElement(Element):
         pm.put(self.ion_beam.xp, p_id_ions, kicked_ions_xp)
         pm.put(self.ion_beam.yp, p_id_ions, kicked_ions_yp)
         # Drift for the ions in one bucket
-        drifted_ions_x = pm.take(self.ion_beam.xp, p_id_ions)*self.L_sep / \
-            (self.ion_beam.mass*c)+pm.take(self.ion_beam.x, p_id_ions)
-        drifted_ions_y = pm.take(self.ion_beam.yp, p_id_ions)*self.L_sep / \
-            (self.ion_beam.mass*c)+pm.take(self.ion_beam.y, p_id_ions)
+        drifted_ions_x = pm.take(
+            self.ion_beam.xp, p_id_ions)*self.L_sep + pm.take(self.ion_beam.x, p_id_ions)
+        drifted_ions_y = pm.take(
+            self.ion_beam.yp, p_id_ions)*self.L_sep + pm.take(self.ion_beam.y, p_id_ions)
         pm.put(self.ion_beam.x, p_id_ions, drifted_ions_x)
         pm.put(self.ion_beam.y, p_id_ions, drifted_ions_y)
-        self.ions_monitor.dump(self.ion_beam)
 
     def get_efieldn(self, xr, yr, mean_x, mean_y, sig_x, sig_y):
         '''The charge-normalised electric field components of a
