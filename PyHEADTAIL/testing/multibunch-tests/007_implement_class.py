c0123953d736289344e6fcc085b7f3fe42288335
giadarol <giovanni.iadarola@cern.ch>
2023-03-13 10:39:59 +0100

    Some definition

--- PyHEADTAIL/testing/multibunch-tests/007_implement_class.py
+++ PyHEADTAIL/testing/multibunch-tests/007_implement_class.py
@@ -8,25 +8,53 @@ class Wakefield:
                 scale_kick,
                 function,
                 z_slice_range=None, # These are [a, b] in the paper
-                n_slices=None, # Per bunch, this is N_1 in the paper
+                num_slices=None, # Per bunch, this is N_1 in the paper
                 slicer=None, # alternatively, a slicer can be used
                 z_period=None, # This is P in the paper
-                n_periods=1, # This is N_S
-                n_turns=1,
+                num_periods=1, # This is N_S
+                num_turns=1,
                 circumference=None):
 
-        N_1 = n_slices
-        P = z_period
-        N_S = n_periods
+        self._N_1 = num_slices # N_1 in the
+        self._P = z_period # P in the paper
+        self._N_S = num_periods # N_S in the paper
+        self.num_turns = num_turns
+
+        self._N_aux = self._N_1 + self._N_2 # N_aux in the paper
 
         # Compute M_aux
-        M_aux = 999 # TODO
+        self._M_aux = (self._N_S + self._N_T - 1) * self._N_aux # M_aux in the paper
 
         self.moments_names = source_moments
-        self.moments_data = np.zeros((M_aux, n_turns, len(source_moments)))
+        self.moments_data = np.zeros(
+            (self._M_aux, self.num_turns, len(source_moments)), dtype=np.float64)
 
         self._G_hat = 'TODO'
         self.scale_kick = scale_kick
+        self.function = function
+
+    @property
+    def num_slices(self):
+        return self._N_1
+
+    @property
+    def num_periods(self):
+        return self._N_S
+
+    @property
+    def z_period(self):
+        return self._P
+
+    @property
+    def _N_2(self):
+        return self._N_1 # For the wakefiled, N_1 = N_2
+
+    @property
+    def _N_T(self):
+        return self._N_S # For the wakefiled, N_S = N_T
+
+    def set_moments(self, i_bunch, i_turn, moments):
+        pass
 
     def track(self, particles):
 
@@ -52,14 +80,6 @@ class Wakefield:
 
         getattr(particles, self.kick)[:] += kick_per_particle
 
-
-
-
-
-
-
-
-
 class ResonatorFunction:
     def __init__(self, Rs, Qs, f0):
         self.Rs = Rs
@@ -70,17 +90,7 @@ class ResonatorFunction:
         res = 0
         return res
 
-
-
-# resonator_function = ResonatorFunction(Rs, Qs, f0)
-
-# # Can be used to plot the function
-# t = np.linspace(-10, 10, 1000)
-# plt.plot(t, resonator_function.evaluate(t))
-
-
 circumference = 26658.8832
-
 rf_bucket_length = 5
 
 # Quadrupolar wakefield
@@ -91,31 +101,19 @@ wf = Wakefield(
     function=ResonatorFunction(Rs=3, Qs=4, f0=5),
     z_slice_range=[-rf_bucket_length/2, rf_bucket_length/2], # These are [a, b] in the paper
     slicer=None, # alternatively, a slicer can be used
-    n_slices=100, # Per bunch, this is N_1 in the paper
+    num_slices=100, # Per bunch, this is N_1 in the paper
     z_period=rf_bucket_length, # This is P in the paper
-    n_periods=1, # This is N_S
-    n_turns=3,
+    num_periods=1, # This is N_S
+    num_turns=3,
     circumference=circumference,
 )
 
-
-# You can always access the slicer
-# wf.slicer # e.g. to use it in a monitor
-
-# You can also access the function
-wf.function # e.g. to plot the wake
-
-wf.track(particles) # This is the tracking function
-
-
-class WakefieldResonator(Wakefield):
-
-    def __init__(self, Rs, Qs, f0, z_slice_range, n_slices):
-        self.Rs = Rs
-        self.Qs = Qs
-        self.f0 = f0
-        self.rf_bucket_length = rf_bucket_length
-        self.circumference = circumference
-        self.function = ResonatorFunction(Rs, Qs, f0)
+wf.set_moments(
+    i_bunch=3,
+    i_turn=2,
+    moments={'num_particles': [1,2,3],
+             'x': [4,5,6],
+             'y': [4,5,6]}
+)
 
 
