51f50085909392f846682f241ed1b6f56a909a15
giadarol <giovanni.iadarola@cern.ch>
2023-03-13 11:59:13 +0100

    Status

--- PyHEADTAIL/testing/multibunch-tests/007_implement_class.py
+++ PyHEADTAIL/testing/multibunch-tests/007_implement_class.py
@@ -11,9 +11,16 @@ class Wakefield:
                 num_slices=None, # Per bunch, this is N_1 in the paper
                 slicer=None, # alternatively, a slicer can be used
                 z_period=None, # This is P in the paper
-                num_periods=1, # This is N_S
+                i_period_range=None, # This is [A, B] in the paper
+                num_periods=None,
                 num_turns=1,
                 circumference=None):
+
+        if i_period_range is not None:
+            raise NotImplementedError('i_period_range is not implemented yet')
+
+        self.dz = (z_slice_range[1] - z_slice_range[0]) / num_slices # h in the paper
+        self._a = z_slice_range[0]
 
         self._N_1 = num_slices # N_1 in the
         self._P = z_period # P in the paper
@@ -53,8 +60,81 @@ class Wakefield:
     def _N_T(self):
         return self._N_S # For the wakefiled, N_S = N_T
 
-    def set_moments(self, i_bunch, i_turn, moments):
-        pass
+    def set_moments(self, i_source, i_turn, moments):
+
+        """
+        Set the moments for a given source and turn.
+
+        Parameters
+        ----------
+        i_source : int
+            The source index, 0 <= i_source < self.num_periods
+        i_turn : int
+            The turn index, 0 <= i_turn < self.num_turns
+        moments : dict
+            A dictionary of the form {moment_name: moment_value}
+
+        """
+
+        assert np.isscalar(i_source)
+        assert np.isscalar(i_turn)
+
+        assert i_source < self._N_S
+        assert i_source >= 0
+
+        assert i_turn < self.num_turns
+        assert i_turn >= 0
+
+        for nn, vv in moments.items():
+            assert nn in self.moments_names, (
+                f'Moment {nn} not in defined moments_names')
+            assert len(vv) == self._N_1, (
+                f'Length of moment {nn} is not equal to num_slices')
+            i_moment = self.moments_names.index(nn)
+            i_start_in_moments_data = self._M_aux - (i_source + 1) * self._N_aux
+            i_end_in_moments_data = i_start_in_moments_data + self._N_1
+
+            self.moments_data[
+                i_start_in_moments_data:i_end_in_moments_data,
+                i_turn, i_moment] = vv
+
+    def get_moment_profile(self, moment_name, i_turn):
+
+        '''
+        Get the moment profile for a given turn.
+
+        Parameters
+        ----------
+        moment_name : str
+            The name of the moment to get
+        i_turn : int
+            The turn index, 0 <= i_turn < self.num_turns
+
+        Returns
+        -------
+        z_out : np.ndarray
+            The z positions within the moment profile
+        moment_out : np.ndarray
+            The moment profile
+        '''
+
+        z_out = np.zeros(self._N_S * self._N_1)
+        moment_out = np.zeros(self._N_S * self._N_1)
+        i_moment = self.moments_names.index(moment_name)
+        for i_source in range(self._N_S):
+            i_start_out = (self._N_S - (i_source + 1)) * self._N_1
+            i_end_out = i_start_out + self._N_1
+            z_out[i_start_out:i_end_out] = (
+                self._a + self.dz / 2
+                - i_source * self._P + self.dz * np.arange(self._N_1))
+
+            i_start_in_moments_data = self._M_aux - (i_source + 1) * self._N_aux
+            i_end_in_moments_data = i_start_in_moments_data + self._N_1
+            moment_out[i_start_out:i_end_out] = (
+                self.moments_data[i_start_in_moments_data:i_end_in_moments_data,
+                                    i_turn, i_moment])
+
+        return z_out, moment_out
 
     def track(self, particles):
 
@@ -91,29 +171,73 @@ class ResonatorFunction:
         return res
 
 circumference = 26658.8832
-rf_bucket_length = 5
+rf_bucket_length = 1
 
 # Quadrupolar wakefield
 wf = Wakefield(
-    source_moments=['charge'], # would be ['charge', 'x'] for dipolar
+    source_moments=['num_particles', 'x', 'y'],
     kick='x',
     scale_kick=[3.4, 'x'], # The kick is scaled by position of the particle for quadrupolar, would be None for dipolar
     function=ResonatorFunction(Rs=3, Qs=4, f0=5),
     z_slice_range=[-rf_bucket_length/2, rf_bucket_length/2], # These are [a, b] in the paper
     slicer=None, # alternatively, a slicer can be used
-    num_slices=100, # Per bunch, this is N_1 in the paper
-    z_period=rf_bucket_length, # This is P in the paper
-    num_periods=1, # This is N_S
+    num_slices=5, # Per bunch, this is N_1 in the paper
+    z_period=10, # This is P in the paper
+    num_periods=4, # This is N_S
     num_turns=3,
     circumference=circumference,
 )
 
+charge_test_0 = np.linspace(-1, 1, 5)
+charge_test_1 = np.linspace(-2, 2, 5)
+charge_test_2 = np.linspace(-3, 3, 5)
+
 wf.set_moments(
-    i_bunch=3,
-    i_turn=2,
-    moments={'num_particles': [1,2,3],
-             'x': [4,5,6],
-             'y': [4,5,6]}
+    i_source=0,
+    i_turn=1,
+    moments={'num_particles': charge_test_0,
+             'x': 2 * charge_test_0,
+             'y': 3 * charge_test_0}
 )
+
+wf.set_moments(
+    i_source=1,
+    i_turn=1,
+    moments={'num_particles': charge_test_1,
+                'x': 2 * charge_test_1,
+                'y': 3 * charge_test_1}
+)
+
+wf.set_moments(
+    i_source=2,
+    i_turn=1,
+    moments={'num_particles': charge_test_2,
+                'x': 2 * charge_test_2,
+                'y': 3 * charge_test_2}
+)
+
+
+z_profile, num_particles_profile = wf.get_moment_profile('num_particles', 1)
+
+assert np.allclose(z_profile,
+    [-30.4, -30.2, -30. , -29.8, -29.6,
+     -20.4, -20.2, -20. , -19.8, -19.6,
+     -10.4, -10.2, -10. ,  -9.8,  -9.6,
+       -0.4, -0.2,   0. ,   0.2,   0.4],
+    rtol=0, atol=1e-12)
+
+assert np.allclose(num_particles_profile,
+    [0. ,  0. ,  0. ,  0. ,  0. ,
+    -3. , -1.5,  0. ,  1.5,  3. ,
+    -2. , -1. ,  0. ,  1. ,  2. ,
+    -1. , -0.5,  0. ,  0.5,  1. ],
+    rtol=0, atol=1e-12)
+
+import matplotlib.pyplot as plt
+plt.close('all')
+plt.plot(z_profile, num_particles_profile, '.')
+
+plt.show()
+
 
 
